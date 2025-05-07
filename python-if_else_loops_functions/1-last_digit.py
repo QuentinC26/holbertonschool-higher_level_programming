072f889b64ddd690b27ded71da90649a6cb84d61
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-07 10:47:34 +0200

    i remove the if except the first if and i replace they if by elif

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 digit = number % 10
 if (digit > 5):
     print(f"Last digit of {number} is {digit} and is greater than 5")
-if (digit == 0):
+elif (digit == 0):
     print(f"Last digit of {number} is {digit} and is 0")
-if (digit != 0 & digit < 6):
+elif (digit != 0 & digit < 6):
     print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
