a9e1576628df6c50b5d65b044bdbb49cefed5a78
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-07 10:20:00 +0200

    replace number by lastnumber in the last condition

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if (lastnumber > 5):
     print(f"Last digit of {number} is {lastnumber} and is greater than 5")
 if (lastnumber == 0):
     print(f"Last digit of {number} is {lastnumber} and is 0")
-if (lastnumber != 0 & number < 6):
+if (lastnumber != 0 & lastnumber < 6):
     print(f"Last digit of {number} is {lastnumber} and is less than 6 and not 0")
