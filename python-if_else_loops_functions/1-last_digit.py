0e528eb00bdc185742056e530220db16ed9fe98e
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-07 10:22:18 +0200

    i replace lastnumber by digit for respect the pycodestyle restrictions

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastnumber = number % 10
-
-if (lastnumber > 5):
-    print(f"Last digit of {number} is {lastnumber} and is greater than 5")
-if (lastnumber == 0):
-    print(f"Last digit of {number} is {lastnumber} and is 0")
-if (lastnumber != 0 & lastnumber < 6):
-    print(f"Last digit of {number} is {lastnumber} and is less than 6 and not 0")
+digit = number % 10
+if (digit > 5):
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+if (digit == 0):
+    print(f"Last digit of {number} is {digit} and is 0")
+if (digit != 0 & digit < 6):
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
