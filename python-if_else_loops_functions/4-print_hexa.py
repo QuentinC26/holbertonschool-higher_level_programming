7a86ec983dbd0a6df8ae3bf549346919e87a6703
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-08 16:06:03 +0200

    replace the f string by  a format method

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numbers in range(0, 99):
-    hexa = hex(numbers)
-    print(f"{numbers} = {hex(numbers)}")
+    hexadecimal = hex(numbers)
+    print("{:d} = {hexadecimal}".format(numbers, hexadecimal))
