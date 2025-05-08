946f4f6b271d3f97c1d0404658e2f4e62e83e160
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-08 19:12:00 +0200

    i remove hex function and i add if condition numbers <= 9 with pass for wait have the solution for print 00 whitout use an other print function

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
-    if (numbers <= 98):
-        hexa = hex(numbers)
-        hexa = hex(numbers).lstrip('0x').upper()
-        print("{:d}, ".format(numbers, hexa), end="")
+    if (numbers <= 9):
+        pass
+    elif (numbers <= 98):
+        print("{}, ".format(numbers), end="")
     elif (numbers == 99):
-        print("{:d}".format(numbers), )
-
-
+        print("{}".format(numbers))
