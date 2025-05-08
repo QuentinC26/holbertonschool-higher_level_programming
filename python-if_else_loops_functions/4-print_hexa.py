2306e93f708399c60ba00cb32db14d290ca86683
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-08 16:07:51 +0200

    i remove the text in the second quote in the print

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numbers in range(0, 99):
     hexadecimal = hex(numbers)
-    print("{:d} = {hexadecimal}".format(numbers, hexadecimal))
+    print("{:d} = {}".format(numbers, hexadecimal))
