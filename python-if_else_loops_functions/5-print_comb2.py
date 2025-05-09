7ba33e78dd72b02c3a5f615c1aba4f9e695cf561
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-09 14:07:10 +0200

    add a :02d in the first format for print 00 to 09

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
-    if (numbers <= 9):
-        pass
-    elif (numbers <= 98):
-        print("{}, ".format(numbers), end="")
+    if (numbers <= 98):
+        print("{:02d}, ".format(numbers), end="")
     elif (numbers == 99):
         print("{}".format(numbers))
