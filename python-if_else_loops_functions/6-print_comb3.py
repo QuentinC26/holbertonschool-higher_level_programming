b56d92c92e7ccde729421db1ceb806e70f59753c
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-09 14:58:52 +0200

    add a variable space, two if condition and elif condition for print two digit and not print the same digit, for example 11

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for numbers in range(0, 10):
+    for numbers_two in range(0, 10):
+        space = ""
+        if (numbers != numbers_two):
+            print("{:d}{:d}, ".format(numbers, numbers_two), end="")
+        elif (numbers == numbers_two):
+            print("{}".format(space), end="")
+        if (numbers == 8 and numbers_two == 9):
+            print("{:d}{:d}".format(numbers, numbers_two))
