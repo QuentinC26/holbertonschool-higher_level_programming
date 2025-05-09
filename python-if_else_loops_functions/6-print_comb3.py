9526dc9002f1444edc64ac6baec4bd62657e722d
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-09 16:09:51 +0200

    i remove a space after the two format in the if condition

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for numbers in range(0, 9):
     for numbers_two in range(numbers + 1, 10):
         if (numbers == 8 and numbers_two == 9):
-            print("{:d}{:d} ".format(numbers, numbers_two))
+            print("{:d}{:d}".format(numbers, numbers_two))
         else:
             print("{:d}{:d}, ".format(numbers, numbers_two), end="")
