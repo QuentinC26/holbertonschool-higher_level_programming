b43fc267ceb051e2850d0ec0c5e4dda76d1dd7d5
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-13 21:58:02 +0200

    remove index in the format

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
 
     for index, arguments in enumerate(my_list, start=1):
-        print("{}:{}".format(index, arguments))
+        print("{}".format(arguments))
