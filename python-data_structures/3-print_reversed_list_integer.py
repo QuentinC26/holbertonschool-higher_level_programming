4d1cd3c537ae620e29f775f9de97a3b63af47e46
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 00:10:25 +0200

    i add a .reverse for activate the inversion of the list and i copy paste the code of task 0 after the .reverse

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+
+    my_list.reverse()
+    my_list
+    for index, arguments in enumerate(my_list, start=1):
+        print("{}".format(arguments))
