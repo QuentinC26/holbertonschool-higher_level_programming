5d08817da6a10aa90737bc1371e6c1c388dc1dc8
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-13 14:06:54 +0200

    i reorganise my code and i  remove two print and im a add a for loop for print the arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
+    argv = sys.argv[1:]
+    number_argument = len(argv)
 
-    number_argument = len(sys.argv[1:])
-
-    if len(sys.argv[0:]):
-        print("{} arguments.".format(number_argument))
-    if number_argument == 1 :
-        print("{} argument:".format(number_argument))
-        print("{}: {}".format(number_argument, sys.argv[1:]))
-    if number_argument >= 2:
+    if number_argument == 0:
+        print("0 arguments.")
+    elif number_argument == 1:
+        print("1 argument:")
+    else:
         print("{} arguments:".format(number_argument))
-        print("{}: {}".format(number_argument, sys.argv[1:]))
+   
+    for index, arguments in enumerate(argv, start=1):
+        print("{}: {}".format(index, arguments))
+       
+    
+
