a31777810162979a5b434b35e83555da92f4432f
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-13 22:22:44 +0200

    i replace my_list by arguments

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
 
     for index, arguments in enumerate(my_list, start=1):
-        print("{}".format(my_list[index - 1]))
+        print("{}".format(arguments))
