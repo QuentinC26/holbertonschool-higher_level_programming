988e766667bc24be590e13fb382b17c27bcb8ace
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-13 23:13:34 +0200

    i add three condition with three return in the for loop

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+
+    for count, arguments in enumerate(my_list, start=1):
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            return idx + 1
