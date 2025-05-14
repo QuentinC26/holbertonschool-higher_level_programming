645be9b26cf29d238053ab3dc134bc00e75a83f1
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 00:39:15 +0200

    add a append operator for include the square value in new_matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
 
-    new_matrix = matrix
+    new_matrix = []
 
     for row in new_matrix:
         # row = ligne horizontal
         for index in range(len(row)):
-            new_matrix = matrix[index][index] * matrix[index][index]
-        if len(row) == 0:
+            if matrix[index][row(index)] * matrix[index][row(index)]:
+                new_matrix.append(True)
+            else:
+                new_matrix.append(False)
+        if len(row) == 0:   
             print("")
     return new_matrix
