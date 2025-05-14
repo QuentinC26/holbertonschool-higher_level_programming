e082d0902c1e59414539c139e438f6de8706255c
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 10:31:40 +0200

    i unite my two condition and i remove else part

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element) :
 
-     for count, arguments in enumerate(my_list, start=1):
-        if idx < 0:
-            return my_list
-        elif idx > len(my_list) - 1:
-            return my_list
-        else:
-            my_list.insert(4, 9)
-            my_list.remove(4)
-            my_list
-            return my_list
+    if 0 < idx < len(my_list):
+        #ce qui correspond à if idx > 0 et if idx < len(my_list)
+        my_list[idx] = element
+        return my_list
+
