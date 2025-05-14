192ffcb76548df2e61553efd76cd140a8c4d0bea
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 17:37:03 +0200

    replace 4 by idx + 1 in the remove

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if idx < 0:
         return my_list
-    my_list.remove(4)
+    my_list.remove(idx + 1)
     my_list
     return my_list
