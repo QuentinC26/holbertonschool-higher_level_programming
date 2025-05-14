c1491e5c6677b94056e511f58afce2ba2ea19397
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 10:39:54 +0200

    i correct my code for respect the pycodestyle restriction

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element) :
+def replace_in_list(my_list, idx, element):
 
     if 0 <= idx < len(my_list):
-        #ce qui correspond à if idx >= 0 et if idx < len(my_list)
+        # Ce qui correspond à if idx >= 0 et if idx < len(my_list)
         my_list[idx] = element
         return my_list
     else:
         return my_list
-
