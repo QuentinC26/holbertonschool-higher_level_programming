c43141c5363bec707f71d2a5d11ec635875d9438
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 10:36:39 +0200

    i add = in the if condition

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element) :
 
-    if 0 < idx < len(my_list):
-        #ce qui correspond à if idx > 0 et if idx < len(my_list)
+    if 0 <= idx < len(my_list):
+        #ce qui correspond à if idx >= 0 et if idx < len(my_list)
         my_list[idx] = element
         return my_list
 
