190d54854b77a0946da59c3ed55fbb29d00d15a5
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-14 10:11:21 +0200

    add format d

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     my_list
     for index, arguments in enumerate(my_list, start=1):
-        print("{}".format(arguments))
+        print("{:d}".format(arguments))
