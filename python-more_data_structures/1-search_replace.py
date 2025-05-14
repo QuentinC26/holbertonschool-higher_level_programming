3b29335f7b831f01089e53d3fe5f35a103af192d
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 01:15:06 +0200

    i remove a space in line 4

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    
+
     for index in my_list:
         if index == search:
             new_list.append(replace)
