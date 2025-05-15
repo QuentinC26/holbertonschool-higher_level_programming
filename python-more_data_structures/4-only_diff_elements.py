cdbdb9c7ce919b27f96ea4c22aead5be41f53fd8
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 10:55:47 +0200

    add a space after the difference operator

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,6 @@
 def only_diff_elements(set_1, set_2):
     different_set_a = set_1.difference(set_2)
     different_set_b = set_2.difference(set_1)
+    
     different_set = different_set_a.union(different_set_b)
     return(different_set)
