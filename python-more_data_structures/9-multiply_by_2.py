7f63f7c4179f53eae5c0151c7940d7d3a3670d44
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-16 00:01:41 +0200

    add a if condition for check if a_dictionary is None, a for loop and a line for add the value a_dictionary * 2 in new_dictionary

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = []
+    
+    if a_dictionary is None:
+        return
+    for index, value in sorted(a_dictionary.items()):
+        new_dictionary.append(value*2)
+    return new_dictionary
