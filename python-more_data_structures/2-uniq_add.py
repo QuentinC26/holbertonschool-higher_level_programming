a558e823a53bb254f9aab7c1a40d30c18d88fc81
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 02:17:25 +0200

    add function list and set, before my_list for retire the same argument of the list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    final_result = sum(my_list, start=0)
-    return final_result
+    final_resultat = sum(list(set(my_list)), start=0)
+    return final_resultat
     
