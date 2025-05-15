fc4d0c10e26b2268ca70b3ee384c7840e168cc49
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-16 00:02:23 +0200

    i remove a space in line 4

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = []
-    
+
     if a_dictionary is None:
         return
     for index, value in sorted(a_dictionary.items()):
