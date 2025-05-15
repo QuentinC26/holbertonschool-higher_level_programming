46f7929f619be7d048c45b8b6e6e033a11666e56
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 10:53:29 +0200

    add the difference set_1 at set_2 and the difference set_2 and set_1. Im add after they 2 lines, the union operator for unite different_set_a and different_set_b

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    different_set_a = set_1.difference(set_2)
+    different_set_b = set_2.difference(set_1)
+    different_set = different_set_a.union(different_set_b)
+    return(different_set)
