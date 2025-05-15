cde35152926d42532b0f1552e01c5e71ab716af7
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-15 09:50:20 +0200

    modify my two for loop and i add a variable new_row, i modify the line append

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
     new_matrix = []
 
-    for row in new_matrix:
-        # row = ligne horizontal
-        for index in range(len(row)):
-            if matrix[index][row(index)] * matrix[index][row(index)]:
-                new_matrix.append(True)
-            else:
-                new_matrix.append(False)
-        if len(row) == 0:   
-            print("")
+    for row in matrix:
+        new_row = []
+        for value in row:
+            new_row.append(value**2)
+        new_matrix.append(new_row)
     return new_matrix
