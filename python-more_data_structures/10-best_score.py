e90439dd92b5cdaa01dd1c099077df8aaf044a58
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-16 14:19:44 +0200

    i replace a_dictionnary is None by a_dictionary == NULL

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary == None:
         return None
     the_best = max(a_dictionary, key=a_dictionary.get)
     return the_best
