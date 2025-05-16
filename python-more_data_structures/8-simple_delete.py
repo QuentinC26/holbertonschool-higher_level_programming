295ed03e4d37dcdb20b6f792e9a2d6547dfb6f9e
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-16 11:28:24 +0200

    i remove a all condition and i replace track by key in line a_dictionary.pop

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key == "":
-        return a_dictionary
-    else:
-        key = a_dictionary.pop("track", None)
-        return a_dictionary
+    a_dictionary.pop(key, None)
+    return a_dictionary
