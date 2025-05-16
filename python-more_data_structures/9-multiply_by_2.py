520f5c709bcc478e0947d3df863972ad7cc04ef3
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-16 14:03:31 +0200

    i replace new_dictionary.append by new_dictionary[key] and i replace index by key

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = []
+    new_dictionary = {}
 
     if a_dictionary is None:
         return
-    for index, value in sorted(a_dictionary.items()):
-        new_dictionary.append(value*2)
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value*2
     return new_dictionary
