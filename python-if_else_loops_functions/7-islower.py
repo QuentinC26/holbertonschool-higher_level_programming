1833c6ea9908b00c154b6557afb9909e411053b8
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-18 18:09:13 +0200

    i add a condition for check if c is lowercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,6 @@
-#!/usr/bin/python3
-islower = __import__('7-islower').islower
-
-
+#!/usr/bin/env python3
 def islower(c):
-    if islower("c"):
+    if ord(c) >= 97 and orc(c) <= 122:
         return True
     else:
         return False
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
