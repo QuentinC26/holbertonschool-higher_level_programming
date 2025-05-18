b4ff24e88db3c5c0fe1a2a3b9b17af6905f5269c
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-18 18:55:36 +0200

    i replace 90 by 91

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for index in str:
-        if ord(index) >= 65 and ord(index) <= 90:
+        if ord(index) >= 65 and ord(index) <= 91:
             print("{}".format(str))
             return True
     print("{}".format(str))
