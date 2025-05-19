88ac161f68d23e44ecbdaa7507d6981817b7c77f
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-19 17:16:19 +0200

    add def init in the classs square

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square Class.
+
+Square class is a class that represents a square.
+This must add Private instance attribute: size
+"""
+
+
+class Square:
+    '''
+    Square Class.
+
+    Square class is a class that represents a square.
+    This must add Private instance attribute: size
+    '''
+    def __init__(self, _Square__size):
+        self._Square__size = _Square__size
