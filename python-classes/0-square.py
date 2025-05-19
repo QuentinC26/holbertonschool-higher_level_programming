e8d19fdc8afda47b332f6397ac1c55ec2d62330f
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-19 16:25:52 +0200

    i resolve a pycodestyle problems

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,7 @@
 class Square:
     '''
     Square Class.
-    
-    The Square class is a class that represents a square but is currently empty.
+
+    Square class is a class that represents a square but is currently empty.
     '''
     pass
