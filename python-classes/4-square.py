3d0add12834d4360577b6103e2d950a0763f4604
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-20 23:45:55 +0200

    i add the property and setter property.I replace the condition in the size setter property, add a return self size in the property and add self.__size = size = value in size setter property

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Square Class.
+
+Square class is a class that represents a square.
+I add Private instance attribute: size
+"""
+
+
+class Square:
+    '''
+    Square Class.
+
+    Square class is a class that represents a square.
+    I add Private instance attribute: size
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter 
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError
+        self.__size = value
+
+    def area(self):
+    # i add self.__size **2 for have the area of Square.
+        return self.__size**2
+    
