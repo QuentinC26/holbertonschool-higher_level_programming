d61745a1fc58ada966a9a9a28f7489fcff57ec8f
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-20 15:28:28 +0200

    replace _Square__size by size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,9 +16,9 @@ class Square:
     This must add Private instance attribute: size
     Im use a try and except for manage the different condition.
     '''
-    def __init__(self, _Square__size=0):
-        if not isinstance(_Square__size, int):
+    def __init__(self, size=0):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if _Square__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self._Square__size = _Square__size
+        self.__size = size
