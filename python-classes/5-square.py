7b3c60b4630e58da37f533128671fb507b28aff4
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 00:47:42 +0200

    add a if condition for check self.size == 0 and the double loop for print the number of # with size

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Square Class.
+
+Square class is a class that represents a square.
+I add Private instance attribute: size
+"""
+
+
+class Square:
+    '''
+    Square Class.
+
+    Square class is a class that represents a square.
+    I add Private instance attribute: size
+    '''
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for horizontal in range(self.size):
+                for vertical in range(self.size):
+                    print("#", end ="")
+                print("")
+
+    def area(self):
+        # i add self.__size **2 for have the area of Square.
+        return self.__size**2
