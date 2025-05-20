463b3f00c64c7df534f92f3f5c88c531634a6a45
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-20 15:55:19 +0200

    add a return self size ** 2 for computes the area ofsquare

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Square Class.
+
+Square class is a class that represents a square.
+I add Private instance attribute: size
+"""
+
+
+class Square:
+    '''
+    Square Class.
+
+    Square class is a class that represents a square.
+    I add Private instance attribute: size
+    '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        #i add self.__size **2 for have the area of Square.
+        return self.__size ** 2
