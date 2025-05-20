3869417ed8ac5376205697567918a004df0fad67
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 01:42:13 +0200

    i add two property for position, add self.position = position in def init and the condition for check self.position

---
+++ python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Square Class.
+
+Square class is a class that represents a square.
+I add Private instance attribute: size
+"""
+
+
+class Square:
+    '''
+    Square Class.
+
+    Square class is a class that represents a square.
+    I add Private instance attribute: size
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        if not len(self.position) == 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not self.position < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for horizontal in range(self.size):
+                for vertical in range(self.size):
+                    print("#", end="")
+                print("")
+        if self.position[1] > 0:
+            print("")
+        
+    def area(self):
+        # i add self.__size **2 for have the area of Square.
+        return self.__size**2
