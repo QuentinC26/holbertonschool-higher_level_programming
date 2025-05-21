a865be1ba53ea0887bcfe5e7da99ab10e3578be1
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 19:13:34 +0200

    i remove a space in line 50

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
             return 0
         else:
             return (self.width + self.height) * 2
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             pass
