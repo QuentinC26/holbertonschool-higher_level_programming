31bcc065409b93be64b17c00c4f9cef5566fcf31
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 02:25:18 +0200

    i create a class and i add pass for create empty class and i add two docstring

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+Rectangle class is a class that represents a rectangle but is empty.
+"""
+
+
+class Rectangle:
+    '''
+    Rectangle Class.
+
+    Rectangle class is a class that represents a rectangle but is empty.
+    '''
+    pass
