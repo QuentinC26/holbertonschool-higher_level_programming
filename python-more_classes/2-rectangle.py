df4944a74882c5cd469d94abbd991aa5e6d84fcb
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 18:49:06 +0200

    i copy the code of 1-rectangle.py and i add a def area(self) part and def perimeter(self) part

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+Rectangle class is a class that represents a rectangle but is empty.
+"""
+
+
+class Rectangle:
+    '''
+    Rectangle Class.
+
+    Rectangle class is a class that represents a rectangle but is empty.
+    '''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if not self.width == 0 or not self.height == 0:
+            return (self.width + self.height) * 2
