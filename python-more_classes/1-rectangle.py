cd193cc54d91d324df9510f804a0d07b477d960d
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 18:12:25 +0200

    i add a init def, a width part and height part

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+Rectangle class is a class that represents a rectangle but is empty.
+"""
+
+
+class Rectangle:
+    '''
+    Rectangle Class.
+
+    Rectangle class is a class that represents a rectangle but is empty.
+    '''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        return self.width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance (value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance (value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
