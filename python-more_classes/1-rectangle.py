ca20c246b0d1c6fd6c65368ca12debe93b4ca284
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-21 18:13:20 +0200

    i correct pycodestyle problem

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,14 +14,14 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.width
-    
+
     @width.setter
     def width(self, value):
-        if not isinstance (value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -30,10 +30,10 @@ class Rectangle:
     @property
     def height(self):
         return self.height
-    
+
     @height.setter
     def height(self, value):
-        if not isinstance (value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
