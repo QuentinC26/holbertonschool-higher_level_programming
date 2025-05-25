3a597b7d1a98a2afdd52831f3b8094e00233c8dc
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-26 00:06:46 +0200

    add a condition for check if last < 0 or not and i use % 10 for print the last digit of number

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = number % 10
+    if last < 0:
+        last = last * (-1)
+        print(last, end="")
+        return last
+    else:
+        print(last, end="")
+        return last
+    
