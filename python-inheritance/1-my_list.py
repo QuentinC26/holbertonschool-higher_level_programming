84bf6692926ea8663814d6fa87037104da9f6c7f
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-26 22:22:57 +0200

    i create new list, i add init part and i add if condition for check if self is int or not

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i want print the list of available attributes and methods of an object.
+"""
+
+
+class MyList(list):
+    """
+    Project Inheritance
+
+    i create a new class.
+    """
+
+
+def __init__(self):
+    super().__init__(self)
+
+
+def print_sorted(self):
+    """
+    Project Inheritance
+
+    i want print the list of available attributes and methods of an object.
+    """
+    if not isinstance(self, int):
+        raise TypeError("Its not a integer")
+    return MyList
