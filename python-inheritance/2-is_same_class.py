c7d767e669bd11551b951c716c6be4f89046df39
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-26 22:43:53 +0200

    i add two condition, in this first condition i add type function

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i want print the list of available attributes and methods of an object.
+"""
+
+def is_same_class(obj, a_class):
+    """
+    Project Inheritance
+
+    i want print the list of available attributes and methods of an object.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
