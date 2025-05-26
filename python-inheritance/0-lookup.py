f4f4016fe8418613ccfd0a647c583fc1de283ea0
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-26 21:50:55 +0200

    i add a commentary after the function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,4 +7,9 @@ i want print the list of available attributes and methods of an object.
 
 
 def lookup(obj):
+    """
+    Project Inheritance
+
+    i want print the list of available attributes and methods of an object.
+    """
     return dir(obj)
