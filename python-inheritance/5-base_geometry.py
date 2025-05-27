6ce5baab7aed387cf6c322cdbd6031b247f261e3
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-27 14:04:25 +0200

    i add new class with pass for create a empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i create a empty class.
+"""
+
+
+class BaseGeometry:
+    """
+    Project Inheritance
+
+    i create a empty class.
+    """
+    pass
