6eec83a2cf35ef1e6e7db67f3c14b1da6588a97b
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-27 16:15:52 +0200

    i copy the code of previous task and i add new class withinit and two condition

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i create a class.
+"""
+
+
+class BaseGeometry:
+    """
+    Project Inheritance
+
+    i create a class.
+    """
+    def area(self):
+        """
+        Project Inheritance
+
+        i create a class.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Project Inheritance
+
+        i create a class.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Project Inheritance
+
+    i create a new class.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        if not isinstance(height, int) or not isinstance(width, int):
+            raise TypeError("height and width are must be an integer")
+        if height <= 0 or width <= 0:
+            raise ValueError("height and must are be greater than 0")
