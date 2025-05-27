0f34ebc92ed22a678abfda3ef2083b7be9ea7df2
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-27 12:11:48 +0200

    i add not type obj condition

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i want print the list of available attributes and methods of an object.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Project Inheritance
+
+    i want print the list of available attributes and methods of an object.
+    """
+    if not type(obj) is a_class:
+        return True
+    else:
+        return False
