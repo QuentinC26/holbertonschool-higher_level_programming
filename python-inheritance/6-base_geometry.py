fa28510e70c34f47fea3c92af7dc97f6e536824b
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-27 15:01:01 +0200

    i add raise Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i create a class.
+"""
+
+
+class BaseGeometry:
+    """
+    Project Inheritance
+
+    i create a class.
+    """
+    def area(self):
+        """
+        Project Inheritance
+
+        i create a class.
+        """
+        raise Exception("area() is not implemented")
