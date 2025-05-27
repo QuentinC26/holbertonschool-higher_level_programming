7e27bd863cfae3540fabbb32fd18218774c46dd2
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-27 15:52:17 +0200

    i correct a pycodestyle problem

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,6 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
 
-
     def integer_validator(self, name, value):
         """
         Project Inheritance
@@ -31,4 +30,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
