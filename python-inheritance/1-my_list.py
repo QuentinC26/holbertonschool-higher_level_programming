6d630493a1f6b517c5b64c98cfaccf876d04659c
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-28 15:41:33 +0200

    i remove the for loop, the return self and def init part

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -12,20 +12,11 @@ class MyList(list):
 
     i create a new class.
     """
-
-
-    def __init__(self):
-        pass
-
-
     def print_sorted(self):
         """
         Project Inheritance
 
         i want print the list of available attributes and methods of an object.
         """
-        for index in self:
-            if not isinstance(index, int):
-                raise TypeError("Its not a integer")
         print(sorted(self))
-        return self
+
