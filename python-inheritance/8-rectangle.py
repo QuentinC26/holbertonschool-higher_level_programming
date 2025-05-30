13d11699220bb5f534b104000ab71705bc4e751d
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-30 21:39:12 +0200

    i add the code of previous task before class rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,6 +6,31 @@ Project Inheritance
 i create a class.
 """
 
+class BaseGeometry:
+    """
+    Project Inheritance
+
+    i create a class.
+    """
+    def area(self):
+        """
+        Project Inheritance
+
+        i create a class.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Project Inheritance
+
+        i create a class.
+        """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """
