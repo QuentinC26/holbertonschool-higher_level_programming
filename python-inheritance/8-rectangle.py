99731ea389edcff810384b54b829b71ad3e94f0b
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-30 22:43:36 +0200

    i replace height by width in the self.integer_validor for width

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -40,6 +40,6 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         self.integer_validator("height", height)
-        self.integer_validator("height", width)
+        self.integer_validator("width", width)
         self.__width = width
         self.__height = height
