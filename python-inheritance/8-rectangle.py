c2de5941cf9f1f428485b899e38dea2905c8b56a
QuentinC26 <q.zuzlewski@gmail.com>
2025-05-30 21:54:34 +0200

    i remove the condition and i add self.integer_validator (height, height) and the same for width

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -39,9 +39,7 @@ class Rectangle(BaseGeometry):
     i create a new class.
     """
     def __init__(self, width, height):
-        if not isinstance(height, int) or not isinstance(width, int):
-            raise TypeError("height must be an integer")
-        if height <= 0 or width <= 0:
-            raise ValueError("height and must are be greater than 0")
+        self.integer_validator("height", height)
+        self.integer_validator("height", width)
         self.__width = width
         self.__height = height
