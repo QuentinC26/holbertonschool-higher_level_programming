dfd78dabc11054902b314ca2ce3ac369f82755ce
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-03 10:52:41 +0200

    i add with and open function and i add line text = len(filename) with the return text

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Function for write a filetext
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Function for write a filetext
+    '''
+    with open(filename, 'w', encoding="UTF-8") as the_text:
+        write_data = the_text.write(filename)
+    text = len(filename)
+    return text
