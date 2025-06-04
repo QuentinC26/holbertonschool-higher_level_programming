1d73f204676a647da8508d3ce6fbc0dcfd4024a0
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-04 23:09:18 +0200

    i add a return json dumps

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -9,6 +9,4 @@ def to_json_string(my_obj):
     '''
     Function for write a filetext
     '''
-    with open('my_obj.json', 'w', encoding="UTF-8") as the_text:
-        json.dumps(the_text, indent=4,ensure_ascii=False)
-    return the_text
+    return json.dumps(my_obj)
