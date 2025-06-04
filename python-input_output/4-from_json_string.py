466f149c280fcf5006c451e053d2814312efde02
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-04 23:21:49 +0200

    i add return json.loads

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+'''
+Function for write a filetext
+'''
+
+
+def from_json_string(my_str):
+    '''
+    Function for write a filetext
+    '''
+    return json.loads(my_str)
