48ec555c570566835b3ff56fcdfe043a38eda4fe
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-06 01:00:08 +0200

    i correct pycodestyle problem

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pickle
 
+
 class CustomObject:
 
     def __init__(self, name, age, is_student):
@@ -11,7 +12,7 @@ class CustomObject:
     def serialize(self, filename):
         with open(filename, 'wb') as text_file:
             write_data = pickle.dumps(text_file)
-    
+
     @classmethod
     def deserialize(cls, filename):
         with open(filename, 'rb') as text_file:
