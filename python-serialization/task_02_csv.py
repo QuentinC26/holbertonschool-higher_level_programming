2f2d3c6a9d325556e3dd2aeb6ceb9224201c0f97
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-06 15:00:42 +0200

    i add two with and open function with the csv format and json format

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import csv 
+import json
+
+def convert_csv_to_json(filename):
+    with open(filename, 'r', encoding='UTF-8') as csv_file:
+        read_data = list(csv.DictReader(csv_file))
+
+    with open(filename, 'w', encoding='UTF-8') as json_file:
+        json.dump(read_data, json_file)
