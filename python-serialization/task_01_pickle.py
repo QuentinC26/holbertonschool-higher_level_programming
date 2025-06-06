2ee4ec57acd32b406fd0d2793c53c1247aec89b2
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-06 13:37:16 +0200

    i correct the pycodestyle problem

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -3,18 +3,28 @@ import pickle
 
 
 class CustomObject:
-
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
 
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
     def serialize(self, filename):
-        with open(filename, 'wb') as text_file:
-            write_data = pickle.dumps(text_file)
+        try:
+            with open(filename, 'wb') as text_file:
+                write_data = pickle.dump(self, text_file)
+        except TypeError:
+            return None
 
     @classmethod
     def deserialize(cls, filename):
-        with open(filename, 'rb') as text_file:
-            read_data = pickle.load(text_file)
-        return read_data
+        try:
+            with open(filename, 'rb') as text_file:
+                read_data = pickle.load(text_file)
+            return read_data
+        except TypeError:
+            return None
