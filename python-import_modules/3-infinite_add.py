6694e2e24fa0d1dd502f0309c2d09214be4a8a77
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-09 01:00:41 +0200

    i add a empty list and i addfor loop with conversion of int for args and i use append to add a int argv in the list empty

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,11 +4,12 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
 
+    my_sum = []
     if len(argv) == 0:
         print("0")
     else:
-        for index in argv:
+        for index in sys.argv[1:]:
             if index != int:
-                print("")
-            else:
-                print(sum(int(argv)))
+                conversion = int(index)
+                my_sum.append(conversion)
+        print(sum(my_sum))
