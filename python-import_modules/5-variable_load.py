364289663334348cc44881778bf92df96a4b3440
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-09 21:09:14 +0200

    i add __name__ == __main__ condition and i replace the import in the condition

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import variable_load_5
-new_a = variable_load_5.a
-print(new_a)
+if __name__ == "__main__":
+    import variable_load_5
+    new_a = variable_load_5.a
+    print(new_a)
