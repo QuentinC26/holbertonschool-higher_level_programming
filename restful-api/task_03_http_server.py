6a6b0beb6918f26598228a0a1019974b8a261663
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 00:44:13 +0200

    i add a text in the print

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,5 +7,5 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    print("Hello, this is a simple API!", PORT)
     httpd.serve_forever()
