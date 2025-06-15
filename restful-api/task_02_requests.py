3e816e437bfb6627816ae61eae106ce49b0f5abc
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-15 17:58:58 +0200

    i replace the link by jsonplaceholder link, and a  print only the title of the dictionnary

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import requests
-response = requests.get("https://api.exemple.com/data")
+import requests, json, csv
+response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 def fetch_and_print_posts():
     if response.status_code == 200:
         print("Status Code:", response.status_code)
         data = response.json()
-        print(data)
+        print(data['title'])
     else:
         print("Status Code:", response.status_code)
 def fetch_and_save_posts():
