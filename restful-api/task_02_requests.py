4c55eb75080f3ef2821a338fa69a3be928accb81
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-15 22:31:12 +0200

    i replace my code in fetch_and_save_posts by pass and i add a for loop with print index and i replace post/1 by posts

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import requests, json, csv
-response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
 def fetch_and_print_posts():
     if response.status_code == 200:
         print("Status Code:", response.status_code)
         data = response.json()
-        print(data['title'])
+        for index in data:
+            print(index['title'])
     else:
         print("Status Code:", response.status_code)
 def fetch_and_save_posts():
