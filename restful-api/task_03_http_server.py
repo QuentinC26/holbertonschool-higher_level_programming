0a43e0775d19796da18c87c597d78c9c22f67025
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 20:14:08 +0200

    i remove all code and i add a port and server adress

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 import http.server
-import socketserver
-import json
-
+ 
 PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Hello, this is a simple API!", PORT)
-    httpd.serve_forever()
+server_address = ("http://localhost", PORT)
