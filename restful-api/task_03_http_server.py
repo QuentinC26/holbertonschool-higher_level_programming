91c682bbe26d543d001b071e3e6b9aa1247b713a
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 20:21:20 +0200

    i add a part of code

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,4 +2,12 @@
 import http.server
  
 PORT = 8000
-server_address = ("http://localhost", PORT)
+server_address = ("", PORT)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print("Serveur actif sur le port :", PORT)
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
