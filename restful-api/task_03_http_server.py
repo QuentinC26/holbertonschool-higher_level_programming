3f925c7838b00ca74529bdc54e8f95c58e9ece79
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 20:22:57 +0200

    i remove all code and i have only the shebang et the import

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 import http.server
  
-PORT = 8000
-server_address = ("", PORT)
-
-server = http.server.HTTPServer
-handler = http.server.CGIHTTPRequestHandler
-handler.cgi_directories = ["/"]
-print("Serveur actif sur le port :", PORT)
-
-httpd = server(server_address, handler)
-httpd.serve_forever()
