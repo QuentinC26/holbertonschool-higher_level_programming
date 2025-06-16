a96c2232977d9bb2e2eb817f577eaf60535812e9
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 23:04:43 +0200

    i add port, adress, serv, handler and httpd in the code for create my http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 import http.server
- 
+
+port = 8000
+adress = ("", port)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print("Hello, this is a simple API!")
+
+httpd = server(adress, handler)
+httpd.serve_forever()
