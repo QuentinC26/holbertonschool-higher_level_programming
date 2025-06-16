f07cd859538f57b9933f85f8837c4541ce45c536
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-16 23:52:23 +0200

    i add a function with four condition for data, info and status

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,4 +10,15 @@ handler = http.server.SimpleHTTPRequestHandler
 print("Hello, this is a simple API!")
 
 httpd = server(adress, handler)
+
+def the_condition(self):
+    if adress == ("http://localhost/data", port):
+        return {"name": "John", "age":30, "city": "New York"}
+    if adress == ("http://localhost/info", port):
+        return {"version": "1.0", "description": "A simple API built with http.server"}
+    if adress == ("http://localhost/info", port):
+        return ok
+    else:
+        return "404 Not Found"
+
 httpd.serve_forever()
