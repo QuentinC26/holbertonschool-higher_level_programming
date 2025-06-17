d4adabd056c0f157d97316c33d3a682f754cfaea
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-17 18:59:35 +0200

    i modify my code and i add a multiple condition

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python3
 import http.server
+import SimpleHTTPServer
 import json
 
-port = 8000
-adress = ("", port)
-
-server = http.server.HTTPServer
-handler = http.server.SimpleHTTPRequestHandler
-
-print("Hello, this is a simple API!")
-
-httpd = server(adress, handler)
-
-class resquest_handler(handler):
-    def the_condition(self):
-        if self.path == ("http://localhost/data", port):
-            return {"name": "John", "age":30, "city": "New York"}
-        if self.path == ("http://localhost/info", port):
-            return {"version": "1.0", "description": "A simple API built with http.server"}
-        if self.path == ("http://localhost/status", port):
-            return ok
+class the_server(http.server.BaseHTTPRequestHandler):
+    def DO_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!")
+        elif self.path == '/data':
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf8'))
+        elif self.path == '/info':
+            info = {
+                "version": 1.0,
+                "description": "A simple API built with http.server"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode('utf8'))
         else:
-            return "404 Not Found"
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Endpoint not found")
 
-httpd.serve_forever()
+if __name__ == "__main__":
+    port = 8000
+    adress = ("", port)
+    server = http.server.HTTPServer
+    handler = http.server.SimpleHTTPRequestHandler
+    httpd = server(adress, handler)
+    httpd.serve_forever()
