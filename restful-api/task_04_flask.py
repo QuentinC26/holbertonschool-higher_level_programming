ea372de3ed5a5b08eef16b03f159677cd42a2f6b
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-19 00:01:08 +0200

    i modify my first condition of post part

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -27,7 +27,7 @@ def get_username(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-    if not data or 'username' not in data:
+    if not data or not data.get('username'):
         return jsonify({"error": "User is required"}), 400
     user = data['username']
     age = data['age']
