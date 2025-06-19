d2f5bbcaea8d4d1124193ad30310356c90a77dfe
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-19 15:52:42 +0200

    i remove the dictionary with jane, john

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,7 +4,7 @@ import json
 
 app = Flask(__name__)
 
-users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
+users = {}
 
 @app.route('/', methods=['GET'])
 def home():
