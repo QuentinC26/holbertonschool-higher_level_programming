a07751815ed3cad3c8a1350dadade9c31d0041b7
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-19 16:33:06 +0200

    i replace users by data in the if condition and i add nof before username

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -28,7 +28,7 @@ def get_username(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-    if not data or "username" in users:
+    if not data or not "username" in data:
         return jsonify({"error": "User is required"}), 400
     username = data["username"]
     users[username] = data
