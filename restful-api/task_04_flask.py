819b8bcda80d908e37d4fa64fde6a70526cf87f4
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-19 17:05:14 +0200

    i add a new condition in the condtion

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -38,7 +38,6 @@ def add_user():
     if not data or "username" not in data or not data["username"]:
         return jsonify({"error": "User is required"}), 400
 
-    username = data["username"]
     users[username] = data
 
     return jsonify({
