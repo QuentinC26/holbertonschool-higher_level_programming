df136016b6ec2de7ef2055a164013afd516e852d
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-19 16:54:47 +0200

    i add a space

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,17 +6,21 @@ app = Flask(__name__)
 
 users = {}
 
+
 @app.route('/', methods=['GET'])
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route('/data', methods=['GET'])
 def get_users():
     return jsonify(list(users.keys()))
 
+
 @app.route('/status', methods=['GET'])
 def get_status():
     return "OK"
+
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -26,18 +30,22 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
+
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
+
     if not data or "username" not in data:
         return jsonify({"error": "User is required"}), 400
 
     username = data["username"]
     users[username] = data
+
     return jsonify({
         "message": "User added",
         "user": data
     }), 201
+
 
 if __name__ == "__main__":
     app.run()
