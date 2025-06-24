9e3c347454aebd29ffa60d77562072a491d539fc
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-24 15:02:36 +0200

    i add a create table part and insert into part

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- i a create the table second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score int
+);
+INSERT INTO second_table(id, name, score)
+VALUES
+('1', 'John', '10'),
+('2', 'Alex', '3'),
+('3', 'Bob', '4'),
+('4', 'George', '8');
