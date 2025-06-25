63cd76808a1a87f4e1de40b799fe362254e7cdb2
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-25 18:07:39 +0200

    i move the unique and primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,6 @@
 -- create the database hbtn_0d_usa and the table cities with different informations
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(ID),
-    UNIQUE(ID)
 )
