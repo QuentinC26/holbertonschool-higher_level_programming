c3b1e5f3a4bfca221e2067ede6bb86a64cfb21ac
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-25 18:11:41 +0200

    i change the order of different keywords

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create the database hbtn_0d_usa and the table cities with different informations
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
 )
