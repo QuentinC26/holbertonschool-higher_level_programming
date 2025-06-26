7a8946b946947df91d0c6328804401cc23045527
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-26 13:00:20 +0200

    i add a where imbriqué

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- lists all cities of California in database hbtn_0d_usa
+Use hbtn_0d_usa;
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
