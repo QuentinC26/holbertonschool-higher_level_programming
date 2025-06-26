1d2f1f2ceaf1514c812130e08c467a18dac78b44
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-26 23:40:07 +0200

    i remove inner of inner join, i modify state_id by id and i change the place of order by

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id ASC;
-INNER JOIN states ON cities.state_id = states.state_id
+SELECT cities.id, cities.name, states.name FROM cities, states;
+JOIN states ON cities.id = states.id;
+ORDER BY cities.id ASC
