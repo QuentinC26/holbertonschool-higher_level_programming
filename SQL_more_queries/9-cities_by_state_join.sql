67bc06ef105918fc0b33930caabc6aed8d6afdae
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-26 22:50:35 +0200

    i add cities before id and ASC in order by

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY id;
+SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id ASC;
