d7b4acd74687d81bd772f0f33e702b6f69c106eb
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-26 22:31:03 +0200

    i add order by

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- list all cities contained in the database
-SELECT id, name, state_id as name FROM cities;
+SELECT id, name, state_id as name FROM cities ORDER BY id;
 INNER JOIN states ON cities.state_id = states.name
