f8b64b9f90c525ce1da49245ce454e97365d67c8
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 12:03:33 +0200

    i add a WHILE LOOP

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,7 @@
 -- list all genres contained in database and dsiplays the number of shows linked to each
-SELECT DISTINCT name as genre, show_id as number_of_shows
-FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+SELECT DISTINCT name as genre, show_id as number_of_shows FROM tv_genres 
+WHILE genre_id IN show_id:
+BEGIN
+{INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id}
+END
 ORDER BY genre DESC;
