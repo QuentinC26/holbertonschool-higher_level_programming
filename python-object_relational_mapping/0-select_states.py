d3cf7c981fdabeac4253616f8881a41107c042c6
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 23:01:14 +0200

    i add import MySQLdb and line with all_cities

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+import MySQLdb 
+all_cities = hbtn_0e_0_usa.connect("mysql username", "mysql password", "database name")
