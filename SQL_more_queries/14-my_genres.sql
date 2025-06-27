91e4bd156a5833122c8d31a18c1289907336ff91
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 16:18:45 +0200

    i remove the when modify the orthography of different words

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,4 @@
 -- Use the database for list all genre of Dexter
 SELECT DISTINCT COUNT(tv_genres.name) as name
-FROM tv_shows_genres LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id;
-WHERE tv_shows.title IS Dexter
-ORDER BY name ASC;
+FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_genres.name ASC;
