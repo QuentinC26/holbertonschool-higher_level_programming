6d1b28b48023e0dabba9570ddfbfefd926c3c31a
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 17:43:43 +0200

    i change the order of my id name

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT title 
 FROM tv_shows 
 INNER JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres on tv_show_genres.show_id = tv_genres.id
+INNER JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
