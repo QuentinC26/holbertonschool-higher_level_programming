460c099d72a7d589588ac4796f4e37917ee57163
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 16:31:44 +0200

    i replace tv_show_genres.show id by tv_show_genres.genre_id

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
 -- Use the database for list all genre of Dexter
 SELECT name
-FROM tv_shows LEFT JOIN tv_genres ON tv_shows.id = tv_genres.id WHERE tv_shows.title = 'Dexter'
+FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.genre_id = 8
 ORDER BY tv_genres.name ASC;
