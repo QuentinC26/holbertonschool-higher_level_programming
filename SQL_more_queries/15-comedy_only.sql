1674ad6046b5c81f1700c30928d168dd48ef8300
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 17:40:22 +0200

    i add 1 select, 1 from, 2 inner join, 1 where and 1 order by

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- list all Comedy shows in th database 
+SELECT title 
+FROM tv_shows 
+INNER JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres on tv_show_genres.show_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
