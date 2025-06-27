9e32ad77f5a410177535113b51457789d066bcc4
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 14:29:27 +0200

    i replace name by number_of_shows and i add when part

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,4 @@
 -- list all genres contained in database and dsiplays the number of shows linked to each
-SELECT DISTINCT name as genre, show_id as number_of_shows FROM tv_genres 
-WHILE genre_id IN show_id:
-BEGIN
-{INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id}
-END
-ORDER BY genre DESC;
+SELECT DISTINCT name as genre, show_id as number_of_shows 
+FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHEN tv_show_genres.show_id = tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
