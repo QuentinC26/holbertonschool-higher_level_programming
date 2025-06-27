3ad4f688ce4e29b1c03e385d30d05f306d1cfecb
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 16:12:33 +0200

    i add where keywords and i modify the left join

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,5 @@
 -- Use the database for list all genre of Dexter
 SELECT DISTINCT COUNT(tv_genres.name) as name
-FROM tv_shows_genre LEFT JOIN tv_genres_id = tv_shows_genre.genre_id WHERE tv_shows.title is Dexter
+FROM tv_shows_genres LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id;
+WHERE tv_shows.title IS Dexter
 ORDER BY name ASC;
