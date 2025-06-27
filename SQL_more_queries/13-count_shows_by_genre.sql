8ff58897148bbab9228bd1029e45cb0283bdcdf9
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 15:39:01 +0200

    i modify the count and the group by for have the good answer

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- list all genres contained in database and dsiplays the number of shows linked to each
-SELECT DISTINCT name as genre, COUNT(tv_shows.id, tv_show_genres.genre_id) as number_of_shows 
+SELECT DISTINCT name as genre, COUNT(tv_show_genres.show_id) as number_of_shows 
 FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_shows.id, tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
