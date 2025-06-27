50d7d98af9ff616690dd3db616dc2c63d09f0309
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 16:51:42 +0200

    i add count and group by line

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,5 @@
 -- Use the database for list all genre of Dexter
-SELECT name
-FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.genre_id = 8
+SELECT COUNT(tv_show_genres.show_id) AS name
+FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id WHERE tv_show_genres.show_id = 8
+GROUP BY (tv_show_genres.genre_id)
 ORDER BY tv_genres.name ASC;
