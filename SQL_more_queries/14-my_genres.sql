ead7e6e080f528eda635041fe39be432a89f8b6e
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 17:01:21 +0200

    i add a second left join

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,7 @@
 -- Use the database for list all genre of Dexter
-SELECT COUNT(tv_show_genres.show_id) AS name
-FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id WHERE tv_show_genres.show_id = 8
-GROUP BY (tv_show_genres.genre_id)
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_shows ON tv_genres.id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
