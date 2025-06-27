ac4a87d707c0d073e654a0aaf04bfed1e2bf1b04
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-27 17:22:18 +0200

    i change the order of inner join and i modify the name of the variable

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Use the database for list all genre of Dexter
 SELECT name
 FROM tv_genres
-LEFT JOIN tv_shows ON tv_genres.id = tv_shows.id
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
