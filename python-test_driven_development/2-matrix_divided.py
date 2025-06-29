4ed5c942580e4694dc1c7c1dd4b3d2f63318c280
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 20:59:57 +0200

    i add two for loop with three condition and i use a round fonction

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for index in row:
+            if not isinstance(index, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if not isinstance(div, (int, float)):
+                raise TypeError("div must be a number")
+            if div == 0:
+                raise ZeroDivisionError("division by zero")
+            result = index / div
+            """round is for have 2 decimals after the float"""
+            good_result = round(result, 2)
+            new_row.append(good_result)
+        new_matrix.append(new_row)
+    return new_matrix
+    
