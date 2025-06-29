396ef5f534242e63870b185e2f881fd071ebcc69
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 15:36:40 +0200

    i modify the dictionnary and i add a for loop with filter and lambda fonction

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string == None:
         return 0
-    roman_conversion : {
+    roman_conversion = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -10,5 +10,7 @@ def roman_to_int(roman_string):
         'C': 100,
         'D': 500,
        'M': 1000
-    }
+       }
+    for index in roman_conversion:
+        roman_string = filter(lambda conversion: int(index), roman_string)
     return roman_string
