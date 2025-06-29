45d118a8d0c1eba6ca3920dc788ebc0e9b5a2e34
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 15:16:22 +0200

    i add a dictionnary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,4 +2,13 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string == None:
         return 0
+    roman_conversion : {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+       'M': 1000
+    }
     return roman_string
