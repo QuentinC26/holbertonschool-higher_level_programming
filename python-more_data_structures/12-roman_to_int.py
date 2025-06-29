4cecdc017162e6804a2c5535d3ef6a85f516e127
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 14:48:16 +0200

    i remove roman_string != and i replace or by and

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string != isinstance(roman_string, str) or roman_string == None:
+    if isinstance(roman_string, str) and roman_string == None:
         return 0
     return roman_string
