0a428b7f5e5a7f5d5e16736da4cae04ebd402c2d
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 14:57:38 +0200

    i replace and by or

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) and roman_string == None:
+    if isinstance(roman_string, str) or roman_string == None:
         return 0
-    return roman_string
+    
+    return roman(roman_string)
