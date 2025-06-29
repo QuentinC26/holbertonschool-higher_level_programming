c82ceb0b87e35408935c05f0a087765ccd8c2b74
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 15:47:20 +0200

    i resolve pycodestyle problem and i add return map

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_conversion = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-       'M': 1000
-       }
-    for index in roman_conversion:
-        roman_string = filter(lambda conversion: int(index), roman_string)
-    return roman_string
+                        'I': 1,
+                        'V': 5,
+                        'X': 10,
+                        'L': 50,
+                        'C': 100,
+                        'D': 500,
+                        'M': 1000
+                        }
+    return map(roman_string, roman_conversion)
