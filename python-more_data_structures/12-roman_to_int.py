e4baa8cdf3acf952980d50fdd395310828b0c349
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 15:00:37 +0200

    i add not before isinstance

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string == None:
         return 0
-    
-    return roman(roman_string)
+    return roman_string
