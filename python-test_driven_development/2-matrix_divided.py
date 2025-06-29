8edd1673fa991756a944f87efb41a91ba43128b7
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 22:18:36 +0200

    i remove a s for respect pycodestyle restriction

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -14,9 +14,13 @@ def matrix_divided(matrix, div):
         new_row = []
         for index in row:
             if len(row) != len(matrix[0]):
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError(
+                    "Each row of the matrix must have the same size"
+                    )
             if not isinstance(index, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of list) of integers/floats"
+                    )
             if not isinstance(div, (int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
