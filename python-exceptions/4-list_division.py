9f869bb0543a772923c379961a88bfefeb6be5fa
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 18:26:13 +0200

    i remove the condition and i replace this conditon by two except

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,13 +7,13 @@ def list_division(my_list_1, my_list_2, list_length):
                 if not index / index_2:
                     return 0
                 result = index / index_2
-            new_list.append(result)
+                new_list.append(result)
         return new_list
     except ZeroDivisionError:
         print("division by 0")
-        if not isinstance(my_list_1, (int, float)) or not isinstance(my_list_2, (int, float)):
-            print("wrong type")
-        if index >= len(my_list_1) or index_2 >= len(my_list_2):
-            print("out of range")
+    except TypeError:
+        print("wrong type")
+    except IndexError:
+        print("out of range")
     finally:
         return new_list
