d57471819dd9613a5062ae5085d02ef08cb5608e
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 21:15:56 +0200

    i a modify my condition for the size

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,12 +13,10 @@ def matrix_divided(matrix, div):
     for row in matrix:
         new_row = []
         for index in row:
-            if not matrix:
-                raise TypeError
-                ("Each row of the matrix must have the same size")
+            if len(row) != len(matrix[0]):
+                raise TypeError("Each row of the matrix must have the same size")
             if not isinstance(index, (int, float)):
-                raise TypeError
-                ("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             if not isinstance(div, (int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
