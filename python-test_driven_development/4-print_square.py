8bbbff8d0bce72d28415b1684acb87729e8c63ff
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-29 23:32:02 +0200

    i add a new loop and a new variable

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,14 +8,14 @@ def print_square(size):
     '''
     prints a square with the character #
     '''
-    triangle = 0
+    triangle = "#"
+    if size == float and size < 0:
+        raise TypeError("size must be an integer") 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == float and size < 0:
-        raise TypeError("size must be an integer") 
-    else:
+    for row in range(size):
         for index in range(size):
-            size = "#"
-            print(size)
+            print(triangle, end="")
+        print(triangle)
