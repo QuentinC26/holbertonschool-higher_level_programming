b7e420de318b28340eca72ec3b53c74686cc0a09
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 20:55:02 +0200

    i remove one space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,4 +10,4 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    print(text.replace(".", ".\n\n\n").replace("?", "?\n\n\n").replace(":", ":\n\n\n"))
+    print(text.replace(".", ".\n").replace("?", "?\n").replace(":", ":\n"))
