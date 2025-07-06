9af5e5ff73b08136838c18ae99cf64edd490efd8
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 21:26:57 +0200

    i add a space after every special caracters

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,4 +10,5 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    print(text.replace(".", ".\n\n").replace("?", "?\n\n").replace(":", ":\n\n"), end="")
+    print(text.replace(". ", ".\n\n").replace("? ", "?\n\n").replace(": ", ":\n\n"), end="")
+
