b9e6dfbba3693b467f895427e449691a34fb5665
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 22:02:44 +0200

    i add a condition if text is not str

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,4 +8,6 @@ def text_indentation(text):
     '''
     print a text with 2 new lines after each of these characters: ., ? and :
     '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     print(text.replace(". ", ".\n\n").replace("? ", "?\n\n").replace(": ", ":\n\n"), end="")
