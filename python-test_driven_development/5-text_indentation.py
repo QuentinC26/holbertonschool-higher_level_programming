43aaab5e91913a0754aeff1548efa0ab1c118aac
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 21:44:04 +0200

    i add a condition and characters

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,10 +5,12 @@ print a text with 2 new lines after each of these characters: ., ? and :
 
 
 def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     return_of_line_after = [".", ":", "?"]
     for index, characters in enumerate(text):
         if characters in return_of_line_after:
-            print(i, end="\n")
+            print(characters, end="\n")
             print()
         elif characters == " ":
             if text[index-1] in return_of_line_after:
