bb6baa735b27cbe2055c2d2abfc74bbdf389bf82
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 21:53:49 +0200

    i add skip space fonction

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,14 +11,18 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     return_of_line_after = [".", ":", "?"]
+    skip_space = False
     for index, characters in enumerate(text):
         if characters in return_of_line_after:
             print(characters, end="\n")
             print()
         elif characters == " ":
             if text[index-1] in return_of_line_after:
+                skip_space = True
                 continue
             else:
                 print(characters, end="")
+                skip_space = False
         else:
             print(characters, end="")
+            skip_space = False
