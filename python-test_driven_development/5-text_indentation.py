6aeb8b8fe52f60fc4ef7c706eb667517522264dc
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 21:11:31 +0200

    i remove different space and one space after the code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,4 +10,4 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    print(text.replace(". ", ".\n\n").replace("? ", "?\n\n").replace(": ", ":\n\n"), end="")
+    print(text.replace(".", ".\n").replace("?", "?\n").replace(":", ":\n"), end="")
