621198b568ccfa4aa3ad3ebeb87198385966d3e7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 21:44:37 +0200

    i add a function documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,9 @@ print a text with 2 new lines after each of these characters: ., ? and :
 
 
 def text_indentation(text):
+    '''
+    print a text with 2 new lines after each of these characters: ., ? and :
+    '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     return_of_line_after = [".", ":", "?"]
