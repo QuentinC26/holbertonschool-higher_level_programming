08631f7da0dafb68e8f2427968d428cf6c9c3d3f
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-11 23:10:15 +0200

    i remove the all const

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const first_line = 'C is fun';
-const second_line = 'Python is cool';
-const third_line = 'JavaScript is amazing';
+first_line = 'C is fun';
+second_line = 'Python is cool';
+third_line = 'JavaScript is amazing';
 console.log(first_line);
 console.log(second_line);
 console.log(third_line);
