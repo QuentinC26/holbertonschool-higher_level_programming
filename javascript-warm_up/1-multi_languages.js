f14a0dd00bb4dec53fad0a6b264fbb8e6899ee34
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-11 23:20:53 +0200

    i add const before all variable

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-first_line = 'C is fun';
-second_line = 'Python is cool';
-third_line = 'JavaScript is amazing';
+const first_line = 'C is fun';
+const second_line = 'Python is cool';
+const third_line = 'JavaScript is amazing';
 console.log(first_line);
 console.log(second_line);
 console.log(third_line);
