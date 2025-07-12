4bfac72fb583e5d57e24d293915d15b58e910bc0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-12 17:41:54 +0200

    i add a creation of token

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -2,14 +2,25 @@
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+import datetime
+import jwt
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+secret_key = secrets.token_urlsafe(32)
 
 Users = {
     "Anissa_du_06": generate_password_hash("bella"),
     "surfeur_du_07": generate_password_hash("pacific_dream")
     }
+
+payload = {
+    'Users': 'test',
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+}
+
+token = jwt.encode(payload, secret_key, algorithm='HS256')
 
 @auth.login_required
 @auth.verify_password
@@ -17,6 +28,9 @@ def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
+
+@app.route('/login')
+@auth.login_required
 
 if __name__ == '__main__':
     app.run(debug=True)
