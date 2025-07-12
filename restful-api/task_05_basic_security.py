a750c531880f8e386b8ffc2843358248dcf0dc3d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-12 19:07:13 +0200

    i add flask import and i modify the dict

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -4,16 +4,23 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 import datetime
+from flask_jwt_extended import JWTManager, create_access_token
 import jwt
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 secret_key = secrets.token_urlsafe(32)
 
-users = {
-    "Anissa_du_06": generate_password_hash("bella"),
-    "surfeur_du_07": generate_password_hash("pacific_dream")
+users = users = {
+    "Anissa_du_06": {
+        "password": generate_password_hash("bella"),
+        "role": "user"
+    },
+    "surfeur_du_07": {
+        "password": generate_password_hash("pacific_dream"),
+        "role": "admin"
     }
+}
 
 payload = {
     'Username': users,
@@ -24,7 +31,6 @@ token = jwt.encode(payload, secret_key, algorithm='HS256')
 verif_token = jwt.decode(token, key=secret_key, algorithms=['HS256', ])
 verif_token.validate()
 
-@auth.login_required
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
