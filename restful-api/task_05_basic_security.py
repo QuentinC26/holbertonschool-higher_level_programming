ef3b67134e999a86b6d4ca46e9254b163a9d34ac
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-12 22:28:15 +0200

    i correct many problem of my code

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -60,29 +60,26 @@ def post_receive_token():
     if not username or not password or username not in users or not check_password_hash(users[username]["password"], password):
         return jsonify({"error": "Invalid credentials"}), 401
     else:
-        access_token = create_access_token(identity={"username": username, "role": users[username]["role"]})
-        return jsonify(access_token=access_token)
+        access_token = create_access_token(identity=username, additional_claims={"role": user["role"]})
+        return jsonify(access_token=access_token), 200
 
 @app.route('/basic-protected')
 @auth.login_required
 def get_basic_protected_authorization():
-    return jsonify({"Basic Auth: Access Granted"})
+    return "Basic Auth: Access Granted"
 
 @app.route('/jwt-protected')
 @jwt_required()
 def get_jwt_protected_authorization():
-    return jsonify({"JWT Auth: Access Granted"})
+    return "JWT Auth: Access Granted"
 
 @app.route('/admin-only')
 @jwt_required()
 def get_admin_only():
-    data_of_admin = get_jwt_identity()
-    role = data_of_admin.get("role")
-    if not role == "admin":
+    claims = get_jwt()
+    if claims.get("role") != "admin":
         return jsonify({"error": "Admin access required"}), 403
-    else:
-        return jsonify({"Admin Access: Granted"})
-
+    return "Admin Access: Granted"
 
 if __name__ == '__main__':
     app.run(debug=True)
