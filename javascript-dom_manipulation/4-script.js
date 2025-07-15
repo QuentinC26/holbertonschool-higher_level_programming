801157242b58ecce6c69e89230ad06a09b31fd90
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-15 16:08:16 +0200

    i replace add_item.onlick by add_item.addEventListener and i modify the my list in the query selector by .my_list

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,12 +1,10 @@
 const add_item = document.getElementById("add_item");
+const list = document.querySelector(".my_list");
 
-function new_item() {
-    const list = document.querySelector('my_list');
+add_item.addEventListener("click", new_item);
+
+function new_item() { 
     const tag = document.createElement("li");
     const text = document.createTextNode("Item");
     tag.appendChild(text);
-    list.appendChild(tag);
-}
-add_item.onclick = function() { 
-    new_item();
- };
+};
