26e0243fe9c85254ae9a923d1df97693849be4a7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-15 16:54:39 +0200

    i remove all text in function new_text and i add document by element line

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -3,7 +3,5 @@ const the_text = document.getElementById("update_header");
 the_text.addEventListener("click", new_text);
 
 function new_text() { 
-    const new_text = the_text.classList;
-    new_text.remove("Update the header");
-    new_text.add("New Header!!!");
+    document.getElementById("update_header").innerHTML = "New Header!!!";
 };
