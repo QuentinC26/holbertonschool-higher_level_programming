62da94405d33340238b0b36834e54bd7c50d9485
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-15 13:59:19 +0200

    i add parenthesis after add_item

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,6 +1,6 @@
 document.addEventListener("click", add_item, false)
 
-function add_item {
+function add_item() {
     document.getElementById("add_item");
     const my_list = element.classList;
     list.add("<li>Item</li>");
