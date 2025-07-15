84df4ce58fd6178eb05b4091d6a8c59c16fecde0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-15 13:59:04 +0200

    i add getelementby id method and add list item

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,5 +1,7 @@
 document.addEventListener("click", add_item, false)
 
-function add_item{
-    
+function add_item {
+    document.getElementById("add_item");
+    const my_list = element.classList;
+    list.add("<li>Item</li>");
 }
