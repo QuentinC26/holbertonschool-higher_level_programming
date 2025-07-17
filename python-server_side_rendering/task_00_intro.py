54ff2a77030f5b6be5eb91622d37889b963a5a29
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-17 16:08:54 +0200

    i change the order of code

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def generate_invitations (template, attendees):
-    if not isinstance(template, str):
-        raise TypeError("Template must be str")
-    if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
-        raise TypeError("Attendees must be list of dict")
     if not template:
         raise ValueError("Template is empty, no output files generated.")
     if not attendees:
         raise ValueError("No data provided, no output files generated.")
+    if not isinstance(template, str):
+        raise TypeError("Template must be str")
+    if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
+        raise TypeError("Attendees must be list of dict")
     for index in attendees:
         if index == "name":
             new_string = template.replace("{name}", index["name"])
