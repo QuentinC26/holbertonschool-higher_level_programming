17f4405138abe8fbcec44aa5193850990c9c76ac
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-17 15:57:08 +0200

    i remove try and except block and i add two condition with value error and i move the exit in this condition

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,24 +4,20 @@ def generate_invitations (template, attendees):
         raise TypeError("Template must be str")
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
-    try:
-        pass
-    except ValueError:
-        print("Template is empty, no output files generated.")
+    if template is None:
+        raise ValueError("Template is empty, no output files generated.")
         exit()
-    try:
-        for index in attendees:
-            if index == "name":
-                new_string = template.replace("{name}", index["name"])
-            elif index == "event_title":
-                new_string = template.replace("{event_title}", index["event_title"])
-            elif index == "event_date":
-                new_string = template.replace("{event_date}", index["event_date"])
-            elif index == "event_location":
-                new_string = template.replace("{event_location}", index["event_location"])
-            elif index == None:
-                new_value = index.replace(None, "N/A")
-    except ValueError:
-        print("No data provided, no output files generated.")
+    if attendees is None:
+        raise ValueError("No data provided, no output files generated.")
         exit()
-    
+    for index in attendees:
+        if index == "name":
+            new_string = template.replace("{name}", index["name"])
+        elif index == "event_title":
+            new_string = template.replace("{event_title}", index["event_title"])
+        elif index == "event_date":
+            new_string = template.replace("{event_date}", index["event_date"])
+        elif index == "event_location":
+            new_string = template.replace("{event_location}", index["event_location"])
+        elif index == None:
+            new_value = index.replace(None, "N/A")
