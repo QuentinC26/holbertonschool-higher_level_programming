fe82c39301d1ac51ee4f47ab70998cdb97833e95
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-17 14:13:36 +0200

    i add Handle Empty Inputs part and Process Each Attended part

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,10 +2,23 @@
 def generate_invitations (template, attendees):
     try:
         pass
-    except:
-        if not isinstance(template, str):
-            TypeError("The template must be string")
-            exit()
-        if not isinstance(attendees, list(dict)):
-            TypeError("Attendees must be a list of dict")
-            exit()
+    except TypeError:
+        print("The template must be string")
+        exit()
+    except ValueError:
+        print("The template must be not empty")
+    try:
+        for index in attendees:
+            if index == "name":
+                template.replace("{name}", index)
+            elif index == "event_title":
+                template.replace("{event_title}", index)
+            elif index == "event_date":
+                template.replace("{event_date}", index)
+            elif index == "event_location":
+                template.replace("{event_location}", index)
+            elif index == "None":
+                attendees.replace("None", "N/A")
+    except TypeError:
+        print("Attendees must be a list of dict")
+        exit()
