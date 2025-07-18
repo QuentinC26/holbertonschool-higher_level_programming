2e498f676b575ab6c242d3a96f84263cbd3efad4
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 11:25:49 +0200

    i modify the for loop and the condition

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -8,16 +8,16 @@ def generate_invitations (template, attendees):
         raise TypeError("Template must be str")
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
-    for index in attendees:
-        if index.get("key") is None:
-            index["key"] = "N/A"
-        if index == "name":
-            new_string = template.replace("{name}", index["name"])
-        if index == "event_title":
-            new_string = template.replace("{event_title}", index["event_title"])
-        if index == "event_date":
-            new_string = template.replace("{event_date}", index["event_date"])
-        if index == "event_location":
-            new_string = template.replace("{event_location}", index["event_location"])
+    for key, value in attendees.items():
+        if key is None:
+            value == "N/A"
+        elif key == "name":
+            new_string = template.replace("{name}", value)
+        elif key == "event_title":
+            new_string = template.replace("{event_title}", value)
+        elif key == "event_date":
+            new_string = template.replace("{event_date}", value)
+        elif key == "event_location":
+            new_string = template.replace("{event_location}", value)
     print(template)
     print(attendees)
