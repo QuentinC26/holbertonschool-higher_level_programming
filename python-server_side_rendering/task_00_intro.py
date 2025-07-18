fefd4fd6bbf1a4d9dd604d980f65e2b89d9460c0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 11:16:59 +0200

    i replace the raise error by print

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def generate_invitations (template, attendees):
     if not template:
-        raise ValueError("Template is empty, no output files generated.")
+        print("Template is empty, no output files generated.")
     if not attendees:
-        raise ValueError("No data provided, no output files generated.")
+        print("No data provided, no output files generated.")
     if not isinstance(template, str):
         raise TypeError("Template must be str")
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
