33dab3184eb3f10bf5df36c9342da61f63d7b32b
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 21:22:33 +0200

    i remove a parenthisis not necessary

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -6,9 +6,9 @@ def generate_invitations (template, attendees):
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
     if not template:
-        return "Template is empty, no output files generated.")
+        return "Template is empty, no output files generated."
     if not attendees:
-        return "No data provided, no output files generated.")
+        return "No data provided, no output files generated."
     for index, attendee in enumerate(attendees, start=1):
         new_template = template
         for key in ["name", "event_title", "event_date", "event_location"]:
