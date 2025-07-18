35df85f1259f88f60397057a988a04e5bf5595f7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 11:51:51 +0200

    i move the declaration new_template = template in the first loop

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -9,16 +9,16 @@ def generate_invitations (template, attendees):
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
     for index in attendees:
+        new_template = template
         for key, value in index.items():
             if value is None:
                 value = "N/A"
             elif key == "name":
-                new_string_01 = template.replace("{name}", value)
+                new_template = new_template.replace("{name}", value)
             elif key == "event_title":
-                new_string_02 = template.replace("{event_title}", value)
+                new_template = new_template.replace("{event_title}", value)
             elif key == "event_date":
-                new_string_03 = template.replace("{event_date}", value)
+                new_template = new_template.replace("{event_date}", value)
             elif key == "event_location":
-                new_string_04 = template.replace("{event_location}", value)
-    print(template)
-    print(attendees)
+                new_template = new_template.replace("{event_location}", value)
+    print(new_template)
