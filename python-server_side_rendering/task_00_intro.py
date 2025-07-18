0493501cdda2117a29aa0fbdd8bf3f16fbb1d42b
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 21:36:54 +0200

    i replace raise by return in tje second isinstance

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,7 +4,7 @@ def generate_invitations (template, attendees):
     if not isinstance(template, str):
         return "Error: Template must be str"
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
-        raise "Error : Attendees must be list of dict"
+        return "Error : Attendees must be list of dict"
     if not template:
         return "Template is empty, no output files generated."
     if not attendees:
