a4666b9bbd8616d87c9e5199cc25322d142ecca7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 11:30:08 +0200

    i add nested loop and i change the name of variable in the condition

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -8,16 +8,17 @@ def generate_invitations (template, attendees):
         raise TypeError("Template must be str")
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
-    for key, value in attendees.items():
-        if key is None:
-            value == "N/A"
-        elif key == "name":
-            new_string = template.replace("{name}", value)
-        elif key == "event_title":
-            new_string = template.replace("{event_title}", value)
-        elif key == "event_date":
-            new_string = template.replace("{event_date}", value)
-        elif key == "event_location":
-            new_string = template.replace("{event_location}", value)
+    for index in attendees:
+        for key, value in index.items():
+            if value is None:
+                value == "N/A"
+            elif key == "name":
+                new_string_01 = template.replace("{name}", value)
+            elif key == "event_title":
+                new_string_02 = template.replace("{event_title}", value)
+            elif key == "event_date":
+                new_string_03 = template.replace("{event_date}", value)
+            elif key == "event_location":
+                new_string_04 = template.replace("{event_location}", value)
     print(template)
     print(attendees)
