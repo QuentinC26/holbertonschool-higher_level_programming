3a68d51e3b0427299f3be624ae5a8fc1b65ed7b2
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-18 21:01:40 +0200

    i replace the return by print

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -6,9 +6,9 @@ def generate_invitations (template, attendees):
     if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
         raise TypeError("Attendees must be list of dict")
     if not template:
-        return("Template is empty, no output files generated.")
+        print("Template is empty, no output files generated.")
     if not attendees:
-        return("No data provided, no output files generated.")
+        print("No data provided, no output files generated.")
     for index, attendee in enumerate(attendees, start=1):
         new_template = template
         for key, value in attendee.items():
@@ -23,8 +23,5 @@ def generate_invitations (template, attendees):
             if key == "event_location":
                 new_template = new_template.replace("{event_location}", value)
         if not os.path.exists(f"output_{index}.txt"):
-            with open(f'output_{index}.txt', 'w') as file:
-                template_content = file.write(new_template)
-        else:
             with open(f'output_{index}.txt', 'w') as file:
                 template_content = file.write(new_template)
