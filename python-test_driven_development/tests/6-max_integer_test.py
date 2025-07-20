054c381d7cc3aa96500b61447d8a718423863ed0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 23:02:29 +0200

    i add a base for the tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
 """Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    pass
+    def test_add_max_integer_at_the_end(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
