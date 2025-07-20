f8245778c9b80130148cde8b9b3541491f7b03ed
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 15:44:41 +0200

    i remove the return inside the for loop

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,8 +3,10 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for index in range(list_length):
         if not isinstance(my_list_1[index], (int, float)):
+            new_list.append(0)
             print("wrong type")
         if not isinstance(my_list_2[index], (int, float)):
+            new_list.append(0)
             print("wrong type")
         try:
             if index == 0:
@@ -16,4 +18,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(result)
-            return new_list
+    return new_list
