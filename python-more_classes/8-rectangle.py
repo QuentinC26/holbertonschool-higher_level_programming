aec499096308d9df8e660016320dcc0e70083be8
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-21 01:47:22 +0200

    i add def bigger or equal method with four condition

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""Rectangle Class.
+
+Rectangle class is a class that represents a rectangle but is empty.
+"""
+
+
+class Rectangle:
+    '''
+    Rectangle Class.
+
+    Rectangle class is a class that represents a rectangle but is empty.
+    '''
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        line = ""
+        list_of_height = []
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            for horizontal in range(self.height):
+                line = str(self.print_symbol) * self.width
+                list_of_height.append(line)
+            return "\n".join(list_of_height)
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not rect_1:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not rect_2:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1 == rect_2:
+            return rect_1
+        else:
+            return rect_1 or rect_2
