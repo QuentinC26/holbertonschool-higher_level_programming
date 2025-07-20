9012ffb24efc0e0f4adc1d8b7faf8967f3ff733e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 15:49:10 +0200

    i change the order of condition before the try and i change the order inside every condition

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,20 +2,21 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for index in range(list_length):
+        if index > len(my_list_1) or index > len(my_list_2):
+            print("out of range")
+            new_list.append(0)
         if not isinstance(my_list_1[index], (int, float)):
-            new_list.append(0)
             print("wrong type")
+            new_list.append(0)
         if not isinstance(my_list_2[index], (int, float)):
-            new_list.append(0)
             print("wrong type")
+            new_list.append(0)
         try:
             if index == 0:
                 new_list.append(0)
             result = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
             print("division by 0")
-        except IndexError:
-            print("out of range")
         finally:
             new_list.append(result)
     return new_list
