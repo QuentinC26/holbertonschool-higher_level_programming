aece2744521b5d237c3402b768f816fb092c8f39
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 15:40:19 +0200

    i modify the order of code

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,19 +2,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for index in range(list_length):
-        if not isinstance(my_list_1, int) or not isinstance(my_list_1, float):
+        if not isinstance(my_list_1[index], (int, float)):
             print("wrong type")
-        if not isinstance(my_list_2, int) or not isinstance(my_list_2, float):
+        if not isinstance(my_list_2[index], (int, float)):
             print("wrong type")
         try:
             if index == 0:
                 new_list.append(0)
-            result = index / index_2
-            new_list.append(result)
-            return new_list
+            result = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
             print("out of range")
         finally:
+            new_list.append(result)
             return new_list
