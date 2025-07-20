e3c2de4ae7133502c313491998bc8bd5693593b8
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 23:52:10 +0200

    i resolve pycodestyle problem

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
     Rectangle class is a class that represents a rectangle but is empty.
     '''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
