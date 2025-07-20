09cd9b97287592f125d4911dff84f7d26f908084
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 15:31:44 +0200

    i replace the double loop by one loop and i move the try except finally later in the code

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    try:
-        new_list = []
-        for index in my_list_1:
-            for index_2 in my_list_2:
-                if index_2 == 0:
-                    new_list.append(0)
-                result = index / index_2
-                new_list.append(result)
-        return new_list
-    except ZeroDivisionError:
-        print("division by 0")
-    except TypeError:
-        print("wrong type")
-    except IndexError:
-        print("out of range")
-    finally:
-        return new_list
+    new_list = []
+    for index in range(list_length):
+        if not isinstance(my_list_1, int) or not isinstance(my_list_1, float):
+            print("wrong type")
+        if not isinstance(my_list_2, int) or not isinstance(my_list_2, float):
+            print("wrong type")
+        try:
+            if index == 0:
+                new_list.append(0)
+            result = index / index_2
+            new_list.append(result)
+            return new_list
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            return new_list
