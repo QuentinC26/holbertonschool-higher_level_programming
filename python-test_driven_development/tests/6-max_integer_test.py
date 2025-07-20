d3e61ae317efd296f77bfda3d1eddf22377c6c03
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 23:12:27 +0200

    i add multiple tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,20 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_add_max_integer_at_the_end(self):
-        pass
+    def test_max_integer_at_the_end(self):
+        max_integer = list[-1]
+    def test_max_integer_at_the_beginning(self):
+        max_integer = list[0]
+    def test_max_integer_in_the_middle(self):
+        max_integer = list[1:-2]
+    def test_one_value_negative(self):
+        list = [4, 2, 3, 4, -5]
+    def test_only_value_negative(self):
+        list = [-4, -2, -3, -4, -5]
+    def test_list_one_element(self):
+        list = [4]
+    def test_list_empty(self):
+        list = []
 
 if __name__ == '__main__':
     unittest.main()
