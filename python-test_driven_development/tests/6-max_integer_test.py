2cec4fac49643ad7ffd83d11918e7161f4f1775c
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 23:15:39 +0200

    i modify the third tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,11 +6,11 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer_at_the_end(self):
-        max_integer = list[-1]
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
     def test_max_integer_at_the_beginning(self):
-        max_integer = list[0]
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
     def test_max_integer_in_the_middle(self):
-        max_integer = list[1:-2]
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
     def test_one_value_negative(self):
         list = [4, 2, 3, 4, -5]
     def test_only_value_negative(self):
