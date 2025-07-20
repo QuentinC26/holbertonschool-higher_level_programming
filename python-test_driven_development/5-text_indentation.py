ee666adcb928e085b31c664c9af01b1549e269e0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-20 17:32:31 +0200

    i add a print

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,6 +16,7 @@ def text_indentation(text):
         if index in ['.', '?', ':']:
             print(new_text.strip())
             print()
+            print()
             new_text = ""
     if new_text.strip():
         print(new_text.strip(), end="")
