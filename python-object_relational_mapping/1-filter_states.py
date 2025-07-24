e3bd6cc5a3cd25ffa63b7175be9bc0ffbf508468
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-24 21:18:06 +0200

    i add like in the ascending.execute

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+import MySQLdb
+import sys
+
+
+class hbtn_0e_0_usa():
+    '''
+    script that lists all states from the database hbtn_0e_0_usa
+    '''
+    if __name__ == "__main__":
+        the_db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+        if len(sys.argv) != 4:
+            print("the sys.argv must be 3")
+            sys.exit(1)
+        ascending = the_db.cursor()
+        ascending.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+            )
+        result = ascending.fetchall()
+        for row in result:
+            print(f"({row[0]}, '{row[1]}')")
