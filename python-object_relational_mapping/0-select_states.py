3b510ca1a952d58b9a36c4d455174b0c47d60fb5
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-24 21:00:20 +0200

    i add a condition for resolve problem sys

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,15 +10,19 @@ class hbtn_0e_0_usa():
     '''
     script that lists all states from the database hbtn_0e_0_usa
     '''
-    the_db = MySQLdb.connect(
-       host="localhost",
-       user= sys.argv[1],
-       password= sys.argv[2],
-       database= sys.argv[3]
-   )
-    ascending = the_db.cursor()
-    ascending.execute(
-                "SELECT DISTINCT * FROM states ORDER BY states.id ASC;"
-                )
-    result = ascending.fetchall()
-    print(result)
+    if __name__ == "__main__":
+        the_db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+        if len(sys.argv) != 4:
+            print("the sys.argv must be 3")
+            sys.exit(1)
+        ascending = the_db.cursor()
+        ascending.execute(
+            "SELECT id, name FROM states ORDER BY id ASC;"
+            )
+        result = ascending.fetchall()
+        print(result)
