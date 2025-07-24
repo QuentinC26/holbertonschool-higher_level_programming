8e9798d7e4c7a70cdb25f254d9c1814458d6aece
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-24 20:29:01 +0200

    i resolve comment and pycodestyle problem

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,18 @@
 '''
 script that lists all states from the database hbtn_0e_0_usa
 '''
-import MySQLdb 
+import MySQLdb
 import sys
 
-'''
-script that lists all states from the database hbtn_0e_0_usa
-'''
+
 class hbtn_0e_0_usa():
-        the_db = MySQLdb.connect(
-                "Hostname", "mysql username", "mysql password", "database name"
+    '''
+    script that lists all states from the database hbtn_0e_0_usa
+    '''
+    the_db = MySQLdb.connect(
+        "Hostname", "mysql username", "mysql password", "database name"
         )
-        ascending = the_db.cursor()
-        ascending.execute(
+    ascending = the_db.cursor()
+    ascending.execute(
                 "SELECT states FROM hbtn_0e_0_usa ORDER BY states.id ASC;"
                 )
