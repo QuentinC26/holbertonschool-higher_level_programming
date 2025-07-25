aacc4810df5c68cd3aed88fad19a2b545ed2ba56
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-25 11:45:17 +0200

    i add code for this task

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+import MySQLdb
+import sys
+
+
+class hbtn_0e_0_usa():
+    '''
+    script that lists all states from the database hbtn_0e_0_usa
+    '''
+    if __name__ == "__main__":
+        the_db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+        if len(sys.argv) != 5:
+            print("the sys.argv must be 4")
+            sys.exit(1)
+        state_name = sys.argv[4]
+        ascending = the_db.cursor()
+        ascending.execute(
+            "SELECT cities.id, cities.name, states.name "
+            "FROM cities INNER JOIN states ON cities.state_id = states.id "
+            "ORDER BY id ASC;"
+            )
+        result = ascending.fetchall()
+        for row in result:
+            
