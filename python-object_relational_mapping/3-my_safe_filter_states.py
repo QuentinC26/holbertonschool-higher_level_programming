158c5e4fb88444bfc508e9e57a2618ea7b255584
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-25 10:10:29 +0200

    i add %s in the ascending.execute

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+import MySQLdb
+import sys
+
+
+class hbtn_0e_0_usa():
+    '''
+    script that lists all states from the database hbtn_0e_0_usa
+    '''
+    if __name__ == "__main__":
+        the_db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+        if len(sys.argv) != 5:
+            print("the sys.argv must be 4")
+            sys.exit(1)
+        state_name_searched = sys.argv[4]
+        ascending = the_db.cursor()
+        ascending.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY id ASC;", (state_name_searched,)
+            )
+        result = ascending.fetchall()
+        for rows in result:
+            print(f"({rows[0]}, '{state_name_searched.format()}')")
