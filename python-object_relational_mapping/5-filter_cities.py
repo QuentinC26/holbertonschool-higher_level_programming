02987ddfc457d5a24d457fac226281e61d39a23d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-25 11:46:41 +0200

    i add state_name_searched line

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ class hbtn_0e_0_usa():
         if len(sys.argv) != 5:
             print("the sys.argv must be 4")
             sys.exit(1)
-        state_name = sys.argv[4]
+        state_name_searched = sys.argv[4]
         ascending = the_db.cursor()
         ascending.execute(
             "SELECT cities.id, cities.name, states.name "
@@ -29,4 +29,4 @@ class hbtn_0e_0_usa():
             )
         result = ascending.fetchall()
         for row in result:
-            
+            print(f"({row[0]}, '{row[1]}', '{row[2]}')")
