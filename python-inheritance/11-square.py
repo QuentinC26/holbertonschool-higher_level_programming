27b4ac364f92fe1c382fa03336e30fdc9f9ba937
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 01:42:16 +0200

    i add method str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i create a class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Project Inheritance
+
+    i create a new class.
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be greater than 0")
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        return self.__size * self.__size
+        
