82d26f8ab4de0cb6feb1ceb77a0b5b2117cbcc7f
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 09:37:01 +0200

    i try to resolve sqlachemy problem

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,21 +9,18 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 import requests
 
-engine = create_engine(f('mysql+mysqldb://{user}:{pwd}@127.0.0.1:3306/{db_name}', pool_pre_ping=True))
-Base.metadata.create_all(engine)
-
 
 class hbtn_0e_0_usa():
     '''
     script that lists all states from the database hbtn_0e_0_usa
     '''
     if __name__ == "__main__":
-        the_db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            password=sys.argv[2],
-            database=sys.argv[3]
-            )
+        user=sys.argv[1]
+        password=sys.argv[2]
+        database=sys.argv[3]
+        url = f"mysql+mysqldb://{user}:{password}@127.0.0.1:3306/{database}"
+        engine = create_engine(url, pool_pre_ping=True)
+        Base.metadata.create_all(engine)
         if len(sys.argv) != 4:
             print("the sys.argv must be 3")
             sys.exit(1)
