ac64ae208edbd883c0c5849c33430f55aaf24ecd
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 01:22:05 +0200

    i class square and two method

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Project Inheritance
+
+i create a class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Project Inheritance
+
+    i create a new class.
+    """
+    def __init__(self, size):
+        super().__init__()
+        self.__size = size 
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__width * self.__height
