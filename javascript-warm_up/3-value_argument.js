cae560a7765ae16284a6cfa1e448651925d2fa4d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 17:38:19 +0200

    i resolve javascript restriction

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,9 +2,9 @@
 const process = require('process');
 
 process.argv.forEach((argument, index) => {
-    if (index === 1) {
-        console.log("No argument")
-    } else if (index === 2) {
-        console.log(argument)
-    }
+  if (index === 1) {
+    console.log("No argument")
+  } else if (index === 2) {
+    console.log(argument)
+  }
 });
