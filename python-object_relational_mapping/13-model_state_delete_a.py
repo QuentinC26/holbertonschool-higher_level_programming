54ec856fa67aba90b75f72eae04e789e189e3b1f
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 14:12:06 +0200

    i modify the line session.query for delete

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+from sqlalchemy import create_engine, Column, String, Integer, insert
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("the sys.argv must be 4")
+        sys.exit(1)
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # create URL Connexion
+    url = f"mysql+mysqldb://{user}:{password}@127.0.0.1:3306/{database}"
+    # create Engine Object
+    engine = create_engine(url, pool_pre_ping=True)
+    # Create table of database
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = (
+            session.query(State)
+            .order_by(State.id)
+            .all()
+        )
+    session.query(State.name).filter(State.name.like('%a%')).delete()
+    session.commit()
+    for state in query:
+        print("{}: {}".format(state.id, state.name))
