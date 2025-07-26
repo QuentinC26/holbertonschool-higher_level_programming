51eea75d19deae3594d9200592777819e00cf8a1
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 00:16:43 +0200

    i reverse the line integer_validator between width and height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -40,8 +40,7 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height): 
         super().__init__()
-        self.integer_validator("height", height)
         self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        self.integer_validator(width, height)
