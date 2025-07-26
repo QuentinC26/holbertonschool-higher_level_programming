76439b027e1ffa63111b23b9cdb2b4485667d8b0
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 21:35:50 +0200

    i remove condition and i add math floor and i replace N by n in the console log

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,10 +5,8 @@ if (process.argv.length === 2) {
   console.log('Not a number');
 } else if (process.argv.length === 3) {
   if (Number(process.argv[2])) {
-    console.log('My Number: %d', process.argv[2]);
-  } else if (parseFloat(process.argv[2])) {
-      console.log('My Number: %d', parseInt(process.argv[2]));
+    console.log('My number: %d', Math.floor(process.argv[2]));
   } else if (isNaN(Number(process.argv[2]))) {
     console.log("Not a number");
-  } 
+  }
 }
