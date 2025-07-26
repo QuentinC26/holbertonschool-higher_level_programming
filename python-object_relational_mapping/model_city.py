c8a8b23f82e6020ddd15e0699e43070e84679b78
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 15:18:32 +0200

    i remove base = declaration(base)

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -6,14 +6,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
-Base = declarative_base()
 
-
-class State(Base):
+class City(Base):
     '''
     Write a python file that contains the class definition of a cities
     '''
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), primary_key=False)
-    state_id = Column("State.id", Integer, foreign_key=True)
+    state_id = Column("State.id", Integer, foreign_keys=['State'])
