2d93503e4f3d67a6bc2a9d7042d83f0b82fa2c36
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 19:15:45 +0200

    i resolve semicolons problem

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 const process = require('process');
 
 if (process.argv.length === 2) {
-  console.log('%s is %s', "undefined", "undefined");
+  console.log('%s is %s', 'undefined', 'undefined');
 } else if (process.argv.length === 3) {
-  console.log('%s is %s', process.argv[2], "undefined");
+  console.log('%s is %s', process.argv[2], 'undefined');
 } else if (process.argv.length === 4) {
   console.log('%s is %s', process.argv[2], process.argv[3]);
 }
