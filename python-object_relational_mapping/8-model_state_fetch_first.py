f8e0564bb985e5f3b1c99a9e2b79ca8ef9677c95
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 09:47:55 +0200

    i resolve pycodestyle problem

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,9 +14,9 @@ class hbtn_0e_0_usa():
     script that lists all states from the database hbtn_0e_0_usa
     '''
     if __name__ == "__main__":
-        user=sys.argv[1]
-        password=sys.argv[2]
-        database=sys.argv[3]
+        user = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
         url = f"mysql+mysqldb://{user}:{password}@127.0.0.1:3306/{database}"
         engine = create_engine(url, pool_pre_ping=True)
         Base.metadata.create_all(engine)
