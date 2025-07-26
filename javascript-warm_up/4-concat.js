c7ebeaba15ef14ddcfd8c488573132a85198cd19
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 19:14:55 +0200

    i reverse process.argv.length and process.argv. I add undefined for process.argv.lenght === 2

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 const process = require('process');
 
 if (process.argv.length === 2) {
-  console.log('%s is %s', process.argv.length[0], process.argv.length[0]);
+  console.log('%s is %s', "undefined", "undefined");
 } else if (process.argv.length === 3) {
-  console.log('%s is %s', process.argv.length[3], process.argv.length[0]);
-} else {
-  console.log('%s is %s', process.argv.length[3], process.argv.length[4]);
+  console.log('%s is %s', process.argv[2], "undefined");
+} else if (process.argv.length === 4) {
+  console.log('%s is %s', process.argv[2], process.argv[3]);
 }
