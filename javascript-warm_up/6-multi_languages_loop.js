ddc28ac0243b79ad587e1a2be0559306bf0f5803
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 21:46:38 +0200

    i add multiples ;

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const one = 'C is fun'
-const two = 'Python is cool'
-const three = 'JavaScript is amazing'
-const unified = one + '\n' + two + '\n' + three
-console.log(unified)
+const one = 'C is fun';
+const two = 'Python is cool';
+const three = 'JavaScript is amazing';
+const unified = one + '\n' + two + '\n' + three;
+console.log(unified);
 
 for (index in unified) {
-    index = 0
+    index = 0;
 }
