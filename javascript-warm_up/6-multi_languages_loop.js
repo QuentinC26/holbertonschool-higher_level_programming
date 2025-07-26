be57c4a9e883906b7bbbc7b16795e9db1ef7ffa7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-26 21:45:29 +0200

    i add a for loop for respect holberton restriction

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -4,3 +4,7 @@ const two = 'Python is cool'
 const three = 'JavaScript is amazing'
 const unified = one + '\n' + two + '\n' + three
 console.log(unified)
+
+for (index in unified) {
+    index = 0
+}
