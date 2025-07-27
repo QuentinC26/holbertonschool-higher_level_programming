5b62c465e5b89f8bbf55257b44fd57feb749aa0e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 01:39:32 +0200

    i modify the value of a and b

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 function add(a, b) {
-  const process = require('process');
-  a = Number(process.argv[3]);
-  b = Number(process.argv[4]);
   return a + b;
 }
 
+const process = require('process');
 if (process.argv.length !== 4) {
   console.log('NaN');
 } else {
-  const result = add();
+  let a = Number(process.argv[2])
+  let b = Number(process.argv[3])
+  const result = add(a, b);
   console.log(result)
 }
