9506f6e682ea3fe9ddf439b2bb1b59853aa39549
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 00:24:26 +0200

    i remove let index = 0 before the loop

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,6 @@ const caracters = 'X';
 if (isNaN(Number(process.argv[2]))) {
   console.log("Missing size");
 }
-let index = 0;
 for (let index = 0; index < Number(process.argv[2]); index++) {
   process.stdout.write(caracters);
 }
