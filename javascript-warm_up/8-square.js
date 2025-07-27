2b0ac74ff4b236a27cd74d656626eb4ed2bba5ce
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 00:41:25 +0200

    i add a second loop

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,11 +2,11 @@
 const process = require('process');
 const caracters = 'X';
 if (isNaN(Number(process.argv[2]))) {
-  console.log("Missing size");
+  console.log('Missing size');
 }
 for (let index = 0; index < Number(process.argv[2]); index++) {
   for (let index_2 = 0; index_2 < Number(process.argv[2]); index_2++) {
     process.stdout.write(caracters);
   }
-  console.log()
+  console.log();
 }
