14d95009eb012951dc864c62a656be9a3a17c281
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 01:10:43 +0200

    i add else if condition

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const process = require('process');
 function add(a, b) {
   if (process.argv.length < 4) {
     console.log('NaN');
-  } else (isNaN(Number(process.argv[4])))
+  } else if (isNaN(Number(process.argv[4])))
     a = Number(process.argv[3]);
     b = Number(process.argv[4]);
     console.log(a + b);
