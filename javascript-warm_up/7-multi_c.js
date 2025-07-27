4e87a6de729174d137c7c5db0bbaa0f73545220e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 23:56:02 +0200

    i add ; after sentence line

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const process = require('process');
-sentence = 'C is fun'
+sentence = 'C is fun';
 for (index = 0; index < Number(process.argv[2]); index++)
   console.log(sentence);
