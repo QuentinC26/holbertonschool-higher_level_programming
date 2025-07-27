371b53a8df7aa0ef6134a1914949f5f100401be3
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-28 01:10:31 +0200

    i remove two condition

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 function add(a, b) {
-  if (isNaN(Number(process.argv[2]))) {
+  if (process.argv.length < 4) {
     console.log('NaN');
-  } else if(process.argv.length === 2) {
-    console.log('NaN');
-  } else if(process.argv.length === 3) {
-    console.log('NaN');
-  } else {
+  } else (isNaN(Number(process.argv[4])))
     a = Number(process.argv[3]);
     b = Number(process.argv[4]);
-    return a + b;
-  }
+    console.log(a + b);
 }
