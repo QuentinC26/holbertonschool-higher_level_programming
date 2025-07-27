79bf1623db437c22d66b5d4e1ed64c49d1b22f7e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 23:58:27 +0200

    i add const and let before variable sentence and index

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-sentence = 'C is fun';
-for (index = 0; index < Number(process.argv[2]); index++) {
+const sentence = 'C is fun';
+for (let index = 0; index < Number(process.argv[2]); index++) {
   console.log(sentence);
 }
