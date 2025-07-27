d5ca64b811972ce51be7a130b8805207ca944dbc
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 23:57:43 +0200

    i add a correction a for loop syntax

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
 sentence = 'C is fun';
-for (index = 0; index < Number(process.argv[2]); index++)
+for (index = 0; index < Number(process.argv[2]); index++) {
   console.log(sentence);
+}
