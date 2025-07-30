f937b76a74570a17ccf4cfda7f8aabd396697796
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 00:58:23 +0200

    i add condition for print 0

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+if (Number(process.argv.length) === 2) {
+  console.log('0');
+} else if (Number(process.argv.length) === 3) {
+  console.log('0');
+} else {
+    PASS
+}
