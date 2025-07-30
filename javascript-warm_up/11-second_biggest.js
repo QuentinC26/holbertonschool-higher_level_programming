ebcb2e8839e197377ce9f30557f863007711dddf
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 01:21:59 +0200

    i add for loop with reverse

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,11 @@ if (Number(process.argv.length) === 2) {
 } else if (Number(process.argv.length) === 3) {
   console.log('0');
 } else {
-    PASS
+    biggest_second = []
+    args = process.argv.slice(2)
+    for (index = 0; index < args.length; index++) {
+          biggest_second.push((args[index]))
+          biggest_second.reverse();
+        }
+   console.log(biggest_second[1])
 }
