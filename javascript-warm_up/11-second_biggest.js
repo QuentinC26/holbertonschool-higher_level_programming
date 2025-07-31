91f9424ac4754646126827b77d5cc426efaaa727
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 16:14:06 +0200

    i respect semicolons restritions

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,8 @@ if (process.argv.length === 2) {
 } else if (process.argv.length === 3) {
   console.log('0');
 } else {
-    args = process.argv.slice(2)
-    const numbers = args.map(arg => Number(arg));
-    numbers.sort((a, b) => a - b)
-    console.log(numbers[numbers.length - 2])
+  args = process.argv.slice(2);
+  const numbers = args.map(arg => Number(arg));
+  numbers.sort((a, b) => a - b);
+  console.log(numbers[numbers.length - 2]);
 }
