7bad6bf5d54c6b850fb612b966ec75e1f75ea409
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 20:56:22 +0200

    i add the name file in the two open

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,10 +5,10 @@ import json
 
 def convert_csv_to_json(csv_file):
     try:
-        with open(csv_file, 'r', encoding='UTF-8') as text_file:
+        with open('data.csv', 'r', encoding='UTF-8') as text_file:
             read_data = list(csv.DictReader(text_file))
-        with open(csv_file, 'w', encoding='UTF-8') as json_file:
-            json.dump(read_data, json_file, indent=4)
+        with open("data.json", 'w', encoding='UTF-8') as written_file:
+            json.dump(read_data, written_file, indent=4)
         return True
     except Exception:
         return False
