5e08621ec6edb6600a006f8f569c2572b14b172d
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-01 01:32:02 +0200

    i add the method and json return

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+class student
+'''
+
+
+class Student():
+    '''
+    class student
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            hidden_dictionnary = {}
+            for index in attrs:
+                if hasattr(self, index):
+                    hidden_dictionnary[index] = getattr(self, index)
+            return hidden_dictionnary
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+       return json
+      
