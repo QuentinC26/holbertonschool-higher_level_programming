c967cada488ec720f85001aa9d413d9d4d66934b
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 20:43:39 +0200

    i add file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,9 +3,12 @@ import csv
 import json
 
 
-def convert_csv_to_json(filename):
-    with open(filename, 'r', encoding='UTF-8') as csv_file:
-        read_data = list(csv.DictReader(csv_file))
-
-    with open(filename, 'w', encoding='UTF-8') as json_file:
-        json.dump(read_data, json_file)
+def convert_csv_to_json(csv_file):
+    try:
+        with open(csv_file, 'r', encoding='UTF-8') as text_file:
+            read_data = list(csv.DictReader(text_file))
+        with open(csv_file, 'w', encoding='UTF-8') as json_file:
+            json.dump(read_data, json_file, indent=4)
+        return True
+    except Exception:
+        return False
