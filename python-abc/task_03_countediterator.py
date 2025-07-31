41844d0c576c9fb97108f19afe7d8569e57c5db9
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 17:04:49 +0200

    i modify the part next

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,19 +2,17 @@
 from abc import ABC, abstractmethod
 
 class CountedIterator(ABC):
-    @abstractmethod
     def __init__(self, iterator, counter=0):
         self.iterator = iter(iterator)
         self.counter = counter
-        super().__init__()
 
     def get_count(self):
         return self.counter
 
     def __next__(self):
         next(self.iterator)
-        if next(self.iterator):
-            counter = counter + 1
-            return counter
-        else:
+        self.counter = counter + 1
+        if not next(self.iterator):
             raise StopIteration("")
+        else:
+            return next(self.iterator)
