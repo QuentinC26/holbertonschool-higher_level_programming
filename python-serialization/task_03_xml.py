ba95cec978568389f82f87bc569923236f496270
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 22:02:11 +0200

    i add all serialize and derialize file

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as ET
+import json
+
+def serialize_to_xml(dictionary, filename):
+    root_element = ET.Element("data")
+    if dictionary is None or len(dictionary) == 0 :
+        raise ValueError("the dictionary is empty !!")
+    for key, values in dictionary.items():
+        child_element = ET.SubElement(root_element, key)
+        child_element.text = str(values)
+    with open(filename, 'wb') as text_file:
+        ET.ElementTree(root_element).write(text_file, encoding='utf-8', xml_declaration=True)
+
+def deserialize_from_xml(filename):
+    new_dictionary = ET.parse(filename)
+    root_element = new_dictionary.getroot()
+    result_dict = {}
+    for child in root_element:
+        result_dict[child.tag] = child.text
+    return result_dict
