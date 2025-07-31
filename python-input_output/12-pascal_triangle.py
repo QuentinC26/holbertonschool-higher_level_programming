ad1c34ce096ee0d3b1fd9607cdffb7f02fc8082a
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-01 01:46:44 +0200

    i add a base code for this task

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+'''
+pascal triangle
+'''
+
+
+def pascal_triangle(n):
+    '''
+    pascal triangle
+    '''
+    pascal = []
+    if n <= 0:
+        return []
+    if not isinstance(n, int):
+        return []
+    else: 
+        return pascal
