008ff2eb59f5b803631656d52f262cf797fa8473
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-31 16:17:11 +0200

    i add const before args

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (process.argv.length === 2) {
 } else if (process.argv.length === 3) {
   console.log('0');
 } else {
-  args = process.argv.slice(2);
+  const args = process.argv.slice(2);
   const numbers = args.map(arg => Number(arg));
   numbers.sort((a, b) => a - b);
   console.log(numbers[numbers.length - 2]);
