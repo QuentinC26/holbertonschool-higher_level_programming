51bc9d55aefad05b5e740a1c5ccdad01c5a3e71c
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-01 23:55:30 +0200

    i resolve pycodestyle problem

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-'''
+"""
 class student
-'''
+"""
 
 
-class Student():
-    '''
+class Student:
+    """
     class student
-    '''
+    """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -25,4 +26,4 @@ class Student():
 
     def reload_from_json(self, json):
         for key, value in json.items():
-           setattr(self, key, value)
+            setattr(self, key, value)
